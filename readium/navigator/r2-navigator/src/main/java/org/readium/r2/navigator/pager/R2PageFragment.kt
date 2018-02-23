cab84244af8aa3921bfdb9b8ae3b54e0b0e2d5da
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-02-23 14:04:29 +0700

    refactored user settings and cleaned up source

--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2PageFragment.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2PageFragment.kt
@@ -1,32 +1,67 @@
 package org.readium.r2.navigator.pager
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import org.readium.r2.navigator.R
 import org.readium.r2.navigator.R2EpubActivity
-import timber.log.Timber
+import org.readium.r2.navigator.UserSettings.Appearance
+import org.readium.r2.navigator.UserSettings.Scroll
 
 
 class R2PageFragment : Fragment() {
 
     private val TAG = this::class.java.simpleName
 
-    val someIdentifier: String?
+    val resourceUrl: String?
         get() = arguments!!.getString("url")
+
+    val bookTitle: String?
+        get() = arguments!!.getString("title")
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val v = inflater.inflate(R.layout.fragment_page, container, false)
+        val prefs = activity?.getSharedPreferences("org.readium.r2.settings", Context.MODE_PRIVATE)
+
+        val appearance_pref = prefs?.getString("appearance", Appearance.Default.toString()) ?: Appearance.Default.toString()
+        when (appearance_pref) {
+            Appearance.Default.toString() -> {
+                (v.findViewById(R.id.book_title) as TextView).setTextColor(Color.parseColor("#000000"))
+            }
+            Appearance.Sepia.toString() -> {
+                (v.findViewById(R.id.book_title) as TextView).setTextColor(Color.parseColor("#000000"))
+            }
+            Appearance.Night.toString() -> {
+                (v.findViewById(R.id.book_title) as TextView).setTextColor(Color.parseColor("#ffffff"))
+            }
+        }
+
+        val scroll_mode = prefs?.getString("scroll", Scroll.Off.toString())
+        when (scroll_mode) {
+            Scroll.On.toString() -> {
+                (v.findViewById(R.id.book_title) as TextView).visibility = View.GONE
+                v.setPadding(0,4,0,4)
+            }
+            Scroll.Off.toString() -> {
+                (v.findViewById(R.id.book_title) as TextView).visibility = View.VISIBLE
+                v.setPadding(0,30,0,30)
+            }
+        }
+
+        (v.findViewById(R.id.book_title) as TextView).setText(bookTitle)
+
         val webView: R2WebView = v!!.findViewById<R2WebView>(R.id.webView) as R2WebView
 
         webView.activity = activity as R2EpubActivity
@@ -48,86 +83,42 @@ class R2PageFragment : Fragment() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
                 try {
-                    (activity as R2EpubActivity).cssOperator.applyAllCSS(view as R2WebView)
+                    (activity as R2EpubActivity).userSettings.applyAllCSS(view as R2WebView)
                 } catch (e: Exception) {
                     //TODO double check this error, a crash happens when scrolling to fast bewteen resources.....
                     // kotlin.TypeCastException: null cannot be cast to non-null type org.readium.r2.navigator.R2EpubActivity
                 }
+
             }
 
         }
-
-        webView.webChromeClient = object : WebChromeClient() {
-            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
-                super.onShowCustomView(view, callback)
-            }
-        }
-
-        webView.setOnScrollStoppedListener(object : R2WebView.OnScrollStoppedListener {
-
-            override fun onScrollStopped() {
-
-                Timber.d(TAG, "scroll has stopped")
-                webView.snap()
-
-            }
-        })
 
         webView.setOnTouchListener(object : View.OnTouchListener {
 
-            internal var startX = 0
-            internal var startY = 0
-//            internal var SCROLL_THRESHOLD = 10f
-
             override fun onTouch(v: View, event: MotionEvent): Boolean {
 
-                if (event.action == MotionEvent.ACTION_DOWN) {
-
-                    startX = event.x.toInt()
-                    startY = event.y.toInt()
-
-
-                } else if (event.action == MotionEvent.ACTION_UP) {
-
-                    webView.startScrollerTask();
-
-                    val viewWidth = webView.getWidth()
-                    val viewHeight = webView.getHeight()
-//                    val x = event.x.toInt()
-//                    val y = event.y.toInt()
-//                    val absX = Math.abs(startX - event.x)
-//                    val absY = Math.abs(startY - event.y)
-
-                    val relativeDistanceX = (event.x - startX) / viewWidth
-                    val relativeDistanceY = (event.y - startY) / viewHeight
-                    val slope = (event.y - startY) / (event.x - startX)
-
-
-                    Timber.d(TAG, "relativeDistanceX", relativeDistanceX.toString())
-                    Timber.d(TAG, "relativeDistanceY", relativeDistanceY.toString())
-                    Timber.d(TAG, "slope", slope.toString())
-
-                }
-                else if (event.action == MotionEvent.ACTION_MOVE) {
-
+                    if (event.action == MotionEvent.ACTION_MOVE) {
+                    if ((activity as R2EpubActivity).userSettings.isVerticalScrollEnabled) {
+                        return false
+                    }
                     return true
                 }
-
                 return false
             }
         })
 
-        webView.loadUrl(someIdentifier)
+        webView.loadUrl(resourceUrl)
 
         return v
     }
 
     companion object {
 
-        fun newInstance(url: String): R2PageFragment {
+        fun newInstance(url: String, title: String): R2PageFragment {
 
             val args = Bundle()
             args.putString("url", url)
+            args.putString("title", title)
             val fragment = R2PageFragment()
             fragment.arguments = args
             return fragment
