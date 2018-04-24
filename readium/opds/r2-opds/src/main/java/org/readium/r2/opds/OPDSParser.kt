5923ebbf295be0a90fec3a266c70bdc36f8c19f1
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-04-23 20:09:28 -0400

    moved promise extension into shared

--- readium/opds/r2-opds/src/main/java/org/readium/r2/opds/OPDSParser.kt
+++ readium/opds/r2-opds/src/main/java/org/readium/r2/opds/OPDSParser.kt
@@ -1,12 +1,7 @@
 package org.readium.r2.opds
 
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.Request
-import com.github.kittinunf.fuel.core.Response
-import com.github.kittinunf.result.Result
 import nl.komponents.kovenant.Promise
-import nl.komponents.kovenant.deferred
-import nl.komponents.kovenant.task
 import nl.komponents.kovenant.then
 import org.joda.time.DateTime
 import org.readium.r2.shared.*
@@ -410,27 +405,3 @@ class OPDSParser {
         }
     }
 }
-
-
-fun Request.promise(): Promise<Triple<Request, Response, ByteArray>, Exception> {
-    val deferred = deferred<Triple<Request, Response, ByteArray>, Exception>()
-    task { response() } success {
-        val (request, response, result) = it
-        when(result) {
-            is Result.Success -> deferred.resolve(Triple(request, response, result.value))
-            is Result.Failure -> deferred.reject(result.error)
-        }
-    } fail {
-        deferred.reject(it)
-    }
-    return deferred.promise
-}
-
-val Response.contentTypeEncoding: String
-    get() = contentTypeEncoding()
-
-fun Response.contentTypeEncoding(default: String = "utf-8"): String {
-    val contentType: String = httpResponseHeaders["Content-Type"]?.first() ?: return default
-    return contentType.substringAfterLast("charset=", default).substringAfter(' ', default)
-}
-
