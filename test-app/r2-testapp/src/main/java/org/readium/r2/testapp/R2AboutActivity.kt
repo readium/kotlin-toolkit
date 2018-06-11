f5c5ecc87be6aae22d755316777b9fb4dc5e8607
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-06-10 22:56:44 -0400

    added about activity

---
+++ test-app/r2-testapp/src/main/java/org/readium/r2/testapp/R2AboutActivity.kt
@@ -0,0 +1,151 @@
+package org.readium.r2.testapp
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+
+class R2AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+        coordinatorLayout {
+            fitsSystemWindows = true
+            lparams(width = matchParent, height = matchParent)
+            padding = dip(10)
+
+            linearLayout {
+                orientation = LinearLayout.VERTICAL
+                lparams(width = matchParent, height = matchParent)
+
+                textView {
+                    padding = dip(10)
+                    topPadding = dip(30)
+                    text = "Version"
+                    textSize = 20f
+                    typeface = Typeface.DEFAULT_BOLD
+                }
+
+                linearLayout {
+                    orientation = LinearLayout.HORIZONTAL
+                    lparams(width = matchParent, height = wrapContent)
+                    weightSum = 2f
+                    textView {
+                        padding = dip(10)
+                        text = "App Version:"
+                        textSize = 18f
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                    textView {
+                        padding = dip(10)
+                        text = "1.0.0"
+                        textSize = 18f
+                        gravity = Gravity.END
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+
+                }
+                linearLayout {
+                    orientation = LinearLayout.HORIZONTAL
+                    lparams(width = matchParent, height = wrapContent)
+                    weightSum = 2f
+                    textView {
+                        padding = dip(10)
+                        text = "GitHub Tag:"
+                        textSize = 18f
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                    textView {
+                        padding = dip(10)
+                        text = "V1.0.0-beta.1"
+                        textSize = 18f
+                        gravity = Gravity.END
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                }
+
+                textView {
+                    padding = dip(10)
+                    topPadding = dip(30)
+                    text = "Copyright"
+                    textSize = 20f
+                    typeface = Typeface.DEFAULT_BOLD
+                }
+                linearLayout {
+                    orientation = LinearLayout.HORIZONTAL
+                    lparams(width = matchParent, height = wrapContent)
+                    weightSum = 2f
+                    textView {
+                        padding = dip(10)
+                        text = "@ 2018 European Digital Reading Lab"
+                        textSize = 18f
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                    textView {
+                        padding = dip(10)
+                        text = ""
+                        textSize = 18f
+                        gravity = Gravity.END
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                }
+                linearLayout {
+                    orientation = LinearLayout.HORIZONTAL
+                    lparams(width = matchParent, height = wrapContent)
+                    weightSum = 2f
+                    textView {
+                        padding = dip(10)
+                        text = "[BSD-3 license]"
+                        textSize = 18f
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                    textView {
+                        padding = dip(10)
+                        text = ""
+                        textSize = 18f
+                        gravity = Gravity.END
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                }
+
+                textView {
+                    padding = dip(10)
+                    topPadding = dip(30)
+                    text = "Acknowledgements"
+                    textSize = 20f
+                    typeface = Typeface.DEFAULT_BOLD
+                }
+                linearLayout {
+                    orientation = LinearLayout.HORIZONTAL
+                    lparams(width = matchParent, height = wrapContent)
+                    weightSum = 2f
+                    textView {
+                        padding = dip(10)
+                        text = "R2 Reader wouldn't have been developed without the financial help of the CNL (Centre National du Livre, France)."
+                        textSize = 18f
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                    textView {
+                        padding = dip(10)
+                        text = ""
+                        textSize = 18f
+                        gravity = Gravity.END
+                    }.lparams(width = wrapContent, height = wrapContent, weight = 1f)
+                }
+                linearLayout {
+                    orientation = LinearLayout.HORIZONTAL
+                    lparams(width = matchParent, height = wrapContent)
+
+                    imageView {
+                        image = getResources().getDrawable( R.drawable.cnl, theme)
+                        scaleType = ImageView.ScaleType.FIT_CENTER
+                    }.lparams(width = wrapContent, height = 200)
+                }
+
+
+            }
+        }
+    }
+
+}
