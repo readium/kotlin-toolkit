d782b514d8048d15815b782b2f9e95c2f11fdc20
Clement Baumann <clement.baumann@epitech.eu>
2018-07-03 17:04:50 +0200

    fix #45 moved all the generic parsers into shared

--- readium/opds/r2-opds/src/main/java/org/readium/r2/opds/OPDS2Parser.kt
+++ readium/opds/r2-opds/src/main/java/org/readium/r2/opds/OPDS2Parser.kt
@@ -43,7 +43,7 @@ class OPDS2Parser {
             return if (isFeed(jsonData)) {
                 ParseData(parseFeed(jsonData, url), null, 2)
             } else {
-                ParseData(null, parsePublication(JSONObject(String(jsonData))), 2)
+                ParseData(null, parsePublication(JSONObject(String(jsonData)), feedUrl), 2)
             }
         }
 
@@ -202,14 +202,6 @@ class OPDS2Parser {
             return link
         }
 
-        internal fun parseIndirectAcquisition(indirectAcquisitionDict: JSONObject) : IndirectAcquisition {
-            val indirectAcquisitionType = indirectAcquisitionDict["type"] as? String ?: throw Exception(OPDS2ParserError.invalidIndirectAcquisition.name)
-            val indirectAcquisition = IndirectAcquisition(typeAcquisition = indirectAcquisitionType)
-            val childDict = indirectAcquisitionDict.getJSONObject("child")
-            val child = parseIndirectAcquisition(indirectAcquisitionDict = childDict)
-            indirectAcquisition.child.add(child)
-            return indirectAcquisition
-        }
 
         internal fun parseFacets(feed: Feed, facets: JSONArray) {
             for (i in 0..(facets.length() - 1)) {
@@ -241,7 +233,7 @@ class OPDS2Parser {
         internal fun parsePublications(feed: Feed, publications: JSONArray) {
             for (i in 0..(publications.length() - 1)) {
                 val pubDict = publications.getJSONObject(i)
-                val pub = parsePublication(pubDict = pubDict)
+                val pub = parsePublication(pubDict, feedUrl)
                 feed.publications.add(pub)
             }
         }
@@ -282,7 +274,7 @@ class OPDS2Parser {
                     val publications = groupDict.getJSONArray("publications") ?: throw Exception(OPDS2ParserError.invalidGroup.name)
                     for (j in 0..(publications.length() - 1)) {
                         val pubDict = publications.getJSONObject(j)
-                        val pub = parsePublication(pubDict)
+                        val pub = parsePublication(pubDict, feedUrl)
                         group.publications.add(pub)
                     }
                 }
@@ -290,250 +282,6 @@ class OPDS2Parser {
             }
         }
 
-        internal fun parsePublication(pubDict: JSONObject) : Publication {
-            val p = Publication()
-
-            if(pubDict.has("metadata")) {
-                pubDict.get("metadata")?.let {
-                    val metadataDict = it as? JSONObject ?: throw Exception(OPDS2ParserError.invalidPublication.name)
-                    val metadata = parsePublicationMetadata(metadataDict = metadataDict)
-                    p.metadata = metadata
-
-                }
-            }
-            if(pubDict.has("links")) {
-                pubDict.get("links")?.let {
-                    val links = it as? JSONArray ?: throw Exception(OPDS2ParserError.invalidPublication.name)
-                    for (i in 0..(links.length() - 1)) {
-                        val linkDict = links.getJSONObject(i)
-                        val link = parseLink(linkDict = linkDict)
-                        p.links.add(link)
-                    }
-                }
-            }
-            if(pubDict.has("images")) {
-                pubDict.get("images")?.let {
-                    val links = it as? JSONArray ?: throw Exception(OPDS2ParserError.invalidPublication.name)
-                    for (i in 0..(links.length() - 1)) {
-                        val linkDict = links.getJSONObject(i)
-                        val link = parseLink(linkDict = linkDict)
-                        p.images.add(link)
-                    }
-                }
-            }
-            return p
-        }
-
-        internal fun parseContributor(cDict: JSONObject) : Contributor {
-            val c = Contributor()
-
-            if (cDict.has("name")){
-                if (cDict.get("name") is String) {
-                    c.multilangName.singleString = cDict.getString("name")
-                } else if (cDict.get("name") is JSONObject) {
-                    val array = cDict.getJSONObject("name")
-                    c.multilangName.multiString = array as MutableMap<String, String>
-                }
-//                        let s as String -> c.multilangName.singleString = s
-//                        let multiString as Map<String, String> -> c.multilangName.multiString = multiString
-//                        else -> throw OPDS2ParserError.invalidContributor
-
-            }
-            if (cDict.has("identifier")){
-                c.identifier = cDict.getString("identifier")
-            }
-            if (cDict.has("sort_as")){
-                c.sortAs = cDict.getString("sort_as")
-            }
-            if (cDict.has("role")){
-                c.roles.add(cDict.getString("role"))
-            }
-            if (cDict.has("links")){
-                val linkDict = cDict.getJSONObject("links")
-                c.links.add(parseLink(linkDict))
-            }
-            return c
-        }
-
-        internal fun parseContributors(contributors: Any) : List<Contributor> {
-            val result: MutableList<Contributor> = mutableListOf()
-            if (contributors is String) {
-                val c = Contributor()
-                c.multilangName.singleString = contributors
-                result.add(c)
-            } else  if (contributors is JSONObject){
-                val c = parseContributor(contributors)
-                result.add(c)
-            } else if (contributors is JSONArray) {
-                for (i in 0..(contributors.length() - 1)) {
-                    val obj = contributors.getJSONObject(i)
-                    val c = parseContributor(obj)
-                    result.add(c)
-                }
-            }
-            return result
-        }
-
-        internal fun parseCollection(collectionDict: JSONObject) : Collection {
-            val name = collectionDict["name"] as? String ?: throw Exception(OPDS2ParserError.invalidCollection.name)
-            val c = Collection(name = name)
-
-            if (collectionDict.has("sort_as")) {
-                c.sortAs = collectionDict.getString("sort_as")
-            }
-            if (collectionDict.has("identifier")) {
-                c.identifier = collectionDict.getString("identifier")
-            }
-            if (collectionDict.has("position")) {
-                c.position = collectionDict.getDouble("position")
-            }
-            if (collectionDict.has("links")) {
-                val links = collectionDict.getJSONArray("links") ?: throw Exception(OPDS2ParserError.invalidCollection.name)
-                for (i in 0..(links.length() - 1)) {
-                    val link = links.getJSONObject(i)
-                    c.links.add(parseLink(linkDict = link))
-                }
-            }
-            return c
-        }
-
-        internal fun parsePublicationMetadata(metadataDict:JSONObject) : Metadata {
-            val m = Metadata()
-            if (metadataDict.has("title")) {
-                m.multilangTitle = MultilangString()
-                m.multilangTitle?.singleString = metadataDict.getString("title")
-            }
-            if (metadataDict.has("identifier")) {
-                m.identifier = metadataDict.getString("identifier")
-            }
-            if (metadataDict.has("@type")) {
-                m.rdfType = metadataDict.getString("@type")
-            } else if (metadataDict.has("type")) {
-                m.rdfType = metadataDict.getString("type")
-            }
-            if (metadataDict.has("modified")) {
-                m.modified = DateTime(metadataDict.getString("modified")).toDate()
-            }
-            if (metadataDict.has("author")) {
-                m.authors.addAll(parseContributors(metadataDict.get("author")))
-            }
-            if (metadataDict.has("translator")) {
-                m.translators.addAll(parseContributors(metadataDict.get("translator")))
-            }
-            if (metadataDict.has("editor")) {
-                m.editors.addAll(parseContributors(metadataDict.get("editor")))
-            }
-            if (metadataDict.has("artist")) {
-                m.artists.addAll(parseContributors(metadataDict.get("artist")))
-            }
-            if (metadataDict.has("illustrator")) {
-                m.illustrators.addAll(parseContributors(metadataDict.get("illustrator")))
-            }
-            if (metadataDict.has("letterer")) {
-                m.letterers.addAll(parseContributors(metadataDict.get("letterer")))
-            }
-            if (metadataDict.has("penciler")) {
-                m.pencilers.addAll(parseContributors(metadataDict.get("penciler")))
-            }
-            if (metadataDict.has("colorist")) {
-                m.colorists.addAll(parseContributors(metadataDict.get("colorist")))
-            }
-            if (metadataDict.has("inker")) {
-                m.inkers.addAll(parseContributors(metadataDict.get("inker")))
-            }
-            if (metadataDict.has("narrator")) {
-                m.narrators.addAll(parseContributors(metadataDict.get("narrator")))
-            }
-            if (metadataDict.has("contributor")) {
-                m.contributors.addAll(parseContributors(metadataDict.get("contributor")))
-            }
-            if (metadataDict.has("publisher")) {
-                m.publishers.addAll(parseContributors(metadataDict.get("publisher")))
-            }
-            if (metadataDict.has("imprint")) {
-                m.imprints.addAll(parseContributors(metadataDict.get("imprint")))
-            }
-            if (metadataDict.has("published")) {
-                m.publicationDate = metadataDict.getString("published")
-            }
-            if (metadataDict.has("description")) {
-                m.description = metadataDict.getString("description")
-            }
-            if (metadataDict.has("source")) {
-                m.source = metadataDict.getString("source")
-            }
-            if (metadataDict.has("rights")) {
-                m.rights = metadataDict.getString("rights")
-            }
-            if (metadataDict.has("subject")) {
-                val subjDict= metadataDict.getJSONArray("subject")
-                for (i in 0..(subjDict.length() - 1)) {
-                    val sub = subjDict.getJSONObject(i)
-                    val subject = Subject()
-                    if (sub.has("name")){
-                        subject.name = sub.getString("name")
-                    }
-                    if (sub.has("sort_as")){
-                        subject.sortAs = sub.getString("sort_as")
-                    }
-                    if (sub.has("scheme")){
-                        subject.scheme = sub.getString("scheme")
-                    }
-                    if (sub.has("code")){
-                        subject.code = sub.getString("code")
-                    }
-                    m.subjects.add(subject)
-                }
-            }
-            if (metadataDict.has("belongs_to")) {
-                val belongsDict = metadataDict.getJSONObject("belongs_to")
-                val belongs = BelongsTo()
-                if (belongsDict.has("series")){
-
-                    if (belongsDict.get("series") is JSONObject){
-                        m.belongsTo?.series?.add(Collection(belongsDict.getString("series")))
-                    } else if (belongsDict.get("series") is JSONArray) {
-                        val array = belongsDict.getJSONArray("series")
-                        for (i in 0..(array.length() - 1)) {
-                            val string = array.getString(i)
-                            m.belongsTo?.series?.add(Collection(string))
-                        }
-                    }
-                }
-
-                if (belongsDict.has("collection")){
-                    if (belongsDict.get("collection") is String){
-                        m.belongsTo?.collection?.add(Collection(belongsDict.getString("collection")))
-                    } else if (belongsDict.get("collection") is JSONObject) {
-                        belongs.series.add(parseCollection(belongsDict.getJSONObject("collection")))
-                    } else if (belongsDict.get("collection") is JSONArray) {
-                        val array = belongsDict.getJSONArray("collection")
-                        for (i in 0..(array.length() - 1)) {
-                            val obj = array.getJSONObject(i)
-                            belongs.series.add(parseCollection(obj))
-                        }
-                    }
-                }
-                m.belongsTo = belongs
-            }
-
-            if (metadataDict.has("duration")) {
-                m.duration = metadataDict.getInt("duration")
-            }
-            if (metadataDict.has("language")) {
-                if (metadataDict.get("language") is JSONObject){
-                    m.languages.add(metadataDict.getString("language"))
-                } else if (metadataDict.get("language") is JSONArray) {
-                    val array = metadataDict.getJSONArray("language")
-                    for (i in 0..(array.length() - 1)) {
-                        val string = array.getString(i)
-                        m.languages.add(string)
-                    }
-                }
-            }
-
-            return m
-        }
     }
 }
 
