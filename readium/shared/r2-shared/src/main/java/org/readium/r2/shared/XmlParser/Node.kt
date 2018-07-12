5f42549081d449c2cbbcd8fb5c2ce8e50928c7e5
mostapha <idmostapha@gmail.com>
2018-07-12 17:07:42 +0200

    r2-shared : Add Copyrights

--- readium/shared/r2-shared/src/main/java/org/readium/r2/shared/XmlParser/Node.kt
+++ readium/shared/r2-shared/src/main/java/org/readium/r2/shared/XmlParser/Node.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2018 Readium Foundation. All rights reserved.
+ */
+
 package org.readium.r2.shared.XmlParser
 
 class Node (val name: String) {
--- readium/shared/r2-shared/src/main/java/org/readium/r2/shared/metadata/BelongsTo.kt
+++ readium/shared/r2-shared/src/main/java/org/readium/r2/shared/metadata/BelongsTo.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2018 Readium Foundation. All rights reserved.
+ */
+
 package org.readium.r2.shared.metadata
 
 import org.readium.r2.shared.Collection
