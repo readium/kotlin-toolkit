1716c037a3dec226f3126aeef233f2316afb261a
mostapha <idmostapha@gmail.com>
2018-07-12 17:06:51 +0200

    r2-navigator : Add Copyrights

--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/UserSettings.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/UserSettings.kt
@@ -1,6 +1,6 @@
-// Copyright 2018 Readium Foundation. All rights reserved.
-// Use of this source code is governed by a BSD-style license which is detailed in the LICENSE file
-// present in the project repository where this source code is maintained.
+/*
+ * Copyright 2018 Readium Foundation. All rights reserved.
+ */
 
 package org.readium.r2.navigator
 
