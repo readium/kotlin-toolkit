4a420df12c6e40de3264943aac6ce064dbf6a244
mostapha <idmostapha@gmail.com>
2018-07-12 16:43:01 +0200

    "r2-testapp : Adding Copyright and BSD-license"

--- test-app/r2-testapp/src/main/java/org/readium/r2/testapp/R2AboutActivity.kt
+++ test-app/r2-testapp/src/main/java/org/readium/r2/testapp/R2AboutActivity.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2018 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by a BSD-style license which is detailed in the
+ * LICENSE file present in the project repository where this source code is maintained.
+ */
+
 package org.readium.r2.testapp
 
 import android.graphics.Typeface
