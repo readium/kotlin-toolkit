10afe5e687e3eefe0de57e8db5fa67b676c00996
mostapha <idmostapha@gmail.com>
2018-07-30 14:50:28 +0200

    Making Locator and Location an JSONable class + Turning LocatorText into a inner class to Locator

--- readium/shared/r2-shared/src/main/java/org/readium/r2/shared/Locator.kt
+++ readium/shared/r2-shared/src/main/java/org/readium/r2/shared/Locator.kt
@@ -8,22 +8,44 @@ package org.readium.r2.shared
 
 
 import org.joda.time.DateTime
+import org.json.JSONObject
 import java.net.URI
 
-class Locator(val publicationId: String, val spineIndex: Integer, val created: DateTime, val title: URI, val locations: MutableList<Location>, val text: LocatorText) {
-    fun toJson(): String{
-        var jsonString = """{ "publicationId": "$publicationId", "spineIndex": "$spineIndex", "created": "$created", "title": "$title", "locations" : [ """
-        if (locations.size > 1) {
-            locations.forEach { jsonString = jsonString + it.toJson() + """, """ }
-        } else {
-            locations.firstOrNull().let { jsonString += """ "${it!!.toJson()}" """ }
+class Locator(val publicationId: String, val spineIndex: Integer, val created: DateTime, val title: URI, val locations: Location, val text: LocatorText?): JSONable {
+
+    override fun getJSON(): JSONObject {
+        val json = JSONObject()
+        json.putOpt("publicationId", publicationId)
+        json.putOpt("spineIndex", spineIndex)
+        json.putOpt("created", created)
+        json.putOpt("title", title)
+        json.putOpt("location", locations.toJson())
+        if (text != null) {
+            json.putOpt("text", text)
         }
-        jsonString += """ ], "text" : "${text.toJson()}" }"""
+        return json
+    }
+
+    fun toJsonString(): String{
+        var jsonString = """{ "publicationId": "$publicationId", "spineIndex": "$spineIndex", "created": "$created", "title": "$title", "locations" : ${locations.toJson()} """
+        if (text != null) { jsonString += """, "text" : "${text.toJson()}"""" }
+        jsonString += """ }"""
         return jsonString
     }
 }
 
-class Location(val id: String, val cfi: String, val css: String, val progression: Double, val position: Integer){
+class Location(val id: String, val cfi: String, val css: String, val progression: Double, val position: Integer): JSONable{
+
+    override fun getJSON(): JSONObject {
+        val json = JSONObject()
+        json.putOpt("id", id)
+        json.putOpt("cfi", cfi)
+        json.putOpt("css", css)
+        json.putOpt("progression", progression)
+        json.putOpt("position", position)
+        return json
+    }
+
     fun toJson(): String {
         var jsonString = """{"""
         id.let { jsonString += """ "id": "$id" """ }
@@ -35,7 +57,16 @@ class Location(val id: String, val cfi: String, val css: String, val progression
     }
 }
 
-class LocatorText(val after: String?, val before: String?, val highlight: String?){
+class LocatorText(val after: String?, val before: String?, val highlight: String?): JSONable{
+
+    override fun getJSON(): JSONObject {
+        val json = JSONObject()
+        json.putOpt("after", after)
+        json.putOpt("before", before)
+        json.putOpt("highlight", highlight)
+        return json
+    }
+
     fun toJson(): String{
         var jsonString =  """{"""
         after.let { jsonString += """ "after": "$after" """ }
@@ -44,3 +75,20 @@ class LocatorText(val after: String?, val before: String?, val highlight: String
         jsonString += """}"""
     }
 }
+/*
+ override fun getJSON(): JSONObject {
+        val json = JSONObject()
+        json.putOpt("title", title)
+        json.putOpt("type", typeLink)
+        json.putOpt("href", href)
+        if (rel.isNotEmpty())
+            json.put("rel", getStringArray(rel))
+        json.putOpt("properties", properties)
+        if (height != 0)
+            json.putOpt("height", height)
+        if (width != 0)
+            json.putOpt("width", width)
+        json.putOpt("duration", duration)
+        return json
+    }
+ */
