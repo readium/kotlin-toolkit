693918dc20627dcfda9cb06045b22d69dbf72b7c
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-07-31 16:36:41 -0400

    added missing functionality

--- readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/LcpLicense.kt
+++ readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/LcpLicense.kt
@@ -32,13 +32,24 @@ class LcpLicense : DrmLicense {
 
     private val TAG = this::class.java.simpleName
 
-    var archivePath: URL
+    var archivePath: URL? = null
     var license: LicenseDocument
     var status: StatusDocument? = null
     var context: DRMContext? = null
     var androidContext:Context
     val lcpHttpService: LcpHttpService = LcpHttpService()
     val database:LcpDatabase
+
+    constructor(lcplData: ByteArray, context: Context) {
+
+        androidContext = context
+
+        database = LcpDatabase(androidContext)
+
+        val data: ByteArray  = lcplData
+        license = LicenseDocument(data)
+
+    }
 
     constructor(path: URL, inArchive: Boolean, context: Context) {
 
@@ -258,6 +269,21 @@ class LcpLicense : DrmLicense {
         ZipUtil.addEntry(tmpZip, lcplFilePath,  licenseURL.openStream().readBytes(),  source);
         tmpZip.delete()
     }
+
+    fun moveLicense(archivePath: String, licenseData: ByteArray) {
+        Log.i(TAG,"LCP moveLicense")
+        val source = File(archivePath)
+        val tmpZip = File(archivePath+".tmp")
+        tmpZip.delete()
+        source.copyTo(tmpZip)
+        source.delete()
+        if (ZipUtil.containsEntry(tmpZip, lcplFilePath)) {
+            ZipUtil.removeEntry(tmpZip, lcplFilePath)
+        }
+        ZipUtil.addEntry(tmpZip, lcplFilePath,  licenseData,  source);
+        tmpZip.delete()
+    }
+
 
     override fun currentStatus(): String {
         Log.i(TAG,"LCP currentStatus")
