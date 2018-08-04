d6ae05997f0f18296d29fe4e747e8b5736762a1f
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-08-04 12:40:59 -0400

    removed lcp commented out code

--- test-app/r2-testapp/src/main/java/org/readium/r2/testapp/CatalogActivity.kt
+++ test-app/r2-testapp/src/main/java/org/readium/r2/testapp/CatalogActivity.kt
@@ -11,9 +11,6 @@
 package org.readium.r2.testapp
 
 
-//import org.readium.r2.lcp.LcpHttpService
-//import org.readium.r2.lcp.LcpLicense
-//import org.readium.r2.lcp.LcpSession
 import android.app.ProgressDialog
 import android.content.ContentResolver
 import android.content.Context
@@ -84,9 +81,9 @@ import java.net.ServerSocket
 import java.net.URL
 import java.util.*
 
-class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClickListener, LcpFunctions {
+open class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClickListener, LcpFunctions {
 
-    private lateinit var server: Server
+    protected lateinit var server: Server
     private var localPort: Int = 0
 
     private lateinit var booksAdapter: BooksAdapter
@@ -573,7 +570,7 @@ class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClickListe
         }
     }
 
-    private fun prepareToServe(pub: PubBox?, fileName: String, absolutePath: String, add: Boolean, lcp: Boolean) {
+    protected fun prepareToServe(pub: PubBox?, fileName: String, absolutePath: String, add: Boolean, lcp: Boolean) {
         if (pub == null) {
             snackbar(catalogView, "Invalid publication")
             return
@@ -692,7 +689,6 @@ class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClickListe
         prepareToServe(pub, book.fileName, file.absolutePath, false, false)
         startActivity(intentFor<R2EpubActivity>("publicationPath" to publicationPath, "epubName" to book.fileName, "publication" to publication))
     }
-
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -819,197 +815,26 @@ class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClickListe
     }
 
 
-
-
-     override fun parseIntentLcpl(uriString: String) {
-//        val uri: Uri? = Uri.parse(uriString)
-//        if (uri != null) {
-//            val progress = indeterminateProgressDialog(getString(R.string.progress_wait_while_downloading_book))
-//            progress.show()
-//            val thread = Thread(Runnable {
-//                val lcpLicense = LcpLicense(URL(uri.toString()).openStream().readBytes(), this)
-//                task {
-//                    lcpLicense.fetchStatusDocument().get()
-//                } then {
-//                    lcpLicense.checkStatus()
-//                    lcpLicense.updateLicenseDocument().get()
-//                } then {
-//                    lcpLicense.areRightsValid()
-//                    lcpLicense.register()
-//                    lcpLicense.fetchPublication()
-//                } then {
-//                    it?.let {
-//                        lcpLicense.moveLicense(it, URL(uri.toString()).openStream().readBytes())
-//                    }
-//                    it!!
-//                } successUi { path ->
-//                    val file = File(path)
-//                    try {
-//                        runOnUiThread({
-//                            val parser = EpubParser()
-//                            val pub = parser.parse(path)
-//                            if (pub != null) {
-//                                val pair = parser.parseRemainingResource(pub.container, pub.publication, pub.container.drm)
-//                                pub.container = pair.first
-//                                pub.publication = pair.second
-//                                prepareToServe(pub, file.name, file.absolutePath, true, true)
-//                                progress.dismiss()
-//
-//                            }
-//                        })
-//                    } catch (e: Throwable) {
-//                        e.printStackTrace()
-//                    }
-//                }
-//            })
-//            thread.start()
-//        }
+    override fun parseIntentLcpl(uriString: String) {
+        // use CatalogActivityPlusLcp for LCP support
+        // update AndroidManifest <activity android:name=".CatalogActivityPlusLcp">
     }
 
-     override fun prepareAndStartActivityWithLCP(drm: Drm, pub: PubBox, book: Book, file: File, publicationPath: String, parser: EpubParser, publication: Publication) {
-//        if (drm.brand == Drm.Brand.Lcp) {
-//            prepareToServe(pub, book.fileName, file.absolutePath, false, true)
-//
-//            handleLcpPublication(publicationPath, drm, {
-//                val pair = parser.parseRemainingResource(pub.container, publication, it)
-//                pub.container = pair.first
-//                pub.publication = pair.second
-//            }, {
-//                if (supportedProfiles.contains(it.profile)) {
-//                    server.addEpub(publication, pub.container, "/" + book.fileName, applicationContext.getExternalFilesDir(null).path + "/styles/UserProperties.json")
-//
-//                    val license = (drm.license as LcpLicense)
-//                    val drmModel = DRMMModel(drm.brand.name,
-//                            license.currentStatus(),
-//                            license.provider().toString(),
-//                            DateTime(license.issued()).toString(DateTimeFormat.shortDateTime()),
-//                            DateTime(license.lastUpdate()).toString(DateTimeFormat.shortDateTime()),
-//                            DateTime(license.rightsStart()).toString(DateTimeFormat.shortDateTime()),
-//                            DateTime(license.rightsEnd()).toString(DateTimeFormat.shortDateTime()),
-//                            license.rightsPrints().toString(),
-//                            license.rightsCopies().toString())
-//
-//                    startActivity(intentFor<R2EpubActivity>("publicationPath" to publicationPath, "epubName" to book.fileName, "publication" to publication, "drmModel" to drmModel))
-//                } else {
-//                    alert(Appcompat, "The profile of this DRM is not supported.") {
-//                        negativeButton("Ok") { }
-//                    }.show()
-//                }
-//            }, {
-//                // Do nothing
-//            }).get()
-//
-//        }
+    override fun prepareAndStartActivityWithLCP(drm: Drm, pub: PubBox, book: Book, file: File, publicationPath: String, parser: EpubParser, publication: Publication) {
+        // use CatalogActivityPlusLcp for LCP support
+        // update AndroidManifest <activity android:name=".CatalogActivityPlusLcp">
     }
 
-     override fun processLcpActivityResult(uri: Uri, it: Uri, progress: ProgressDialog) {
-//        val input = contentResolver.openInputStream(uri)
-//
-//        val thread = Thread(Runnable {
-//            val lcpLicense = LcpLicense(input.readBytes(), this)
-//            task {
-//                lcpLicense.fetchStatusDocument().get()
-//            } then {
-//                lcpLicense.checkStatus()
-//                lcpLicense.updateLicenseDocument().get()
-//            } then {
-//                lcpLicense.areRightsValid()
-//                lcpLicense.register()
-//                lcpLicense.fetchPublication()
-//            } then {
-//                it?.let {
-//                    lcpLicense.moveLicense(it, contentResolver.openInputStream(uri).readBytes())
-//                }
-//                it!!
-//            } successUi { path ->
-//                val file = File(path)
-//                try {
-//                    runOnUiThread({
-//                        val parser = EpubParser()
-//                        val pub = parser.parse(path)
-//                        if (pub != null) {
-//                            val pair = parser.parseRemainingResource(pub.container, pub.publication, pub.container.drm)
-//                            pub.container = pair.first
-//                            pub.publication = pair.second
-//                            prepareToServe(pub, file.name, file.absolutePath, true, true)
-//                            progress.dismiss()
-//                        }
-//                    })
-//                } catch (e: Throwable) {
-//                    e.printStackTrace()
-//                }
-//            }
-//        })
-//        thread.start()
+    override fun processLcpActivityResult(uri: Uri, it: Uri, progress: ProgressDialog) {
+        // use CatalogActivityPlusLcp for LCP support
+        // update AndroidManifest <activity android:name=".CatalogActivityPlusLcp">
     }
-
-//     override fun handleLcpPublication(publicationPath: String, drm: Drm, parsingCallback: (drm: Drm) -> Unit, callback: (drm: Drm) -> Unit, callbackUI: () -> Unit): Promise<Unit, Exception> {
-//           val lcpHttpService = LcpHttpService()
-//           val session = LcpSession(publicationPath, this)
-//
-//           fun validatePassphrase(passphraseHash: String): Promise<LcpLicense, Exception> {
-//               return task {
-//                   lcpHttpService.certificateRevocationList("http://crl.edrlab.telesec.de/rl/EDRLab_CA.crl").get()
-//               } then { pemCrtl ->
-//                   session.resolve(passphraseHash, pemCrtl).get()
-//               }
-//           }
-//
-//           fun promptPassphrase(reason: String? = null, callback: (pass: String) -> Unit) {
-//               runOnUiThread {
-//                   val hint = session.getHint()
-//                   alert(Appcompat, hint, reason ?: "LCP Passphrase") {
-//                       var editText: EditText? = null
-//                       customView {
-//                           verticalLayout {
-//                               textInputLayout {
-//                                   editText = editText { }
-//                               }
-//                           }
-//                       }
-//                       positiveButton("OK") {
-//                           task {
-//                               editText!!.text.toString()
-//                           } then { clearPassphrase ->
-//                               val passphraseHash = HASH.sha256(clearPassphrase)
-//                               session.checkPassphrases(listOf(passphraseHash))
-//                           } then { validPassphraseHash ->
-//                               session.storePassphrase(validPassphraseHash)
-//                               callback(validPassphraseHash)
-//                           }
-//                       }
-//                       negativeButton("Cancel") { }
-//                   }.show()
-//               }
-//           }
-//
-//           return task {
-//               val passphrases = session.passphraseFromDb()
-//               passphrases?.let {
-//                   val lcpLicense = validatePassphrase(it).get()
-//                   drm.license = lcpLicense
-//                   drm.profile = session.getProfile()
-//                   parsingCallback(drm)
-//                   callback(drm)
-//               } ?: run {
-//                   promptPassphrase(null, {
-//                       val lcpLicense = validatePassphrase(it).get()
-//                       drm.license = lcpLicense
-//                       drm.profile = session.getProfile()
-//                       parsingCallback(drm)
-//                       callback(drm)
-//                       callbackUI()
-//                   })
-//               }
-//           }
-//       }
 
 }
 
 interface LcpFunctions {
-     fun parseIntentLcpl(uriString: String)
-     fun prepareAndStartActivityWithLCP(drm: Drm, pub: PubBox, book: Book, file: File, publicationPath: String, parser: EpubParser, publication: Publication)
-     fun processLcpActivityResult(uri: Uri, it: Uri, progress: ProgressDialog)
-//     fun handleLcpPublication(publicationPath: String, drm: Drm, parsingCallback: (drm: Drm) -> Unit, callback: (drm: Drm) -> Unit, callbackUI: () -> Unit): Promise<Unit, Exception>
+    fun parseIntentLcpl(uriString: String)
+    fun prepareAndStartActivityWithLCP(drm: Drm, pub: PubBox, book: Book, file: File, publicationPath: String, parser: EpubParser, publication: Publication)
+    fun processLcpActivityResult(uri: Uri, it: Uri, progress: ProgressDialog)
 }
 
