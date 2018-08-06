991b45e40bb4e59765bc63ab277a4f94fbd61a2c
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-08-06 19:44:40 -0400

    minor fix

--- test-app/r2-testapp/src/main/java/org/readium/r2/testapp/CatalogActivity.kt
+++ test-app/r2-testapp/src/main/java/org/readium/r2/testapp/CatalogActivity.kt
@@ -90,7 +90,7 @@ open class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClick
     private lateinit var opdsDownloader: OPDSDownloader
     private lateinit var publication: Publication
 
-    private lateinit var catalogView: RecyclerView
+    protected lateinit var catalogView: RecyclerView
     private lateinit var alertDialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -675,6 +675,8 @@ open class CatalogActivity : AppCompatActivity(), BooksAdapter.RecyclerViewClick
                 else -> null
             }
         } then {
+            progress.dismiss()
+        } fail {
             progress.dismiss()
         }
     }
