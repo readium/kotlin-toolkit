49fce82e7d7d496a33d45fc96aea69c6fd8f606b
mostapha <idmostapha@gmail.com>
2018-08-10 18:00:02 +0200

    Introducing to LocatorUtils, meant to hold all the functions related to Locators ( getCurrentLocator, goToLocator, .. )

---
+++ readium/shared/r2-shared/src/main/java/org/readium/r2/shared/LocatorUtils.kt
@@ -0,0 +1,33 @@
+/*
+ * Module: r2-shared-kotlin
+ * Developers: Aferdita Muriqi, Mostapha Idoubihi, Paul Stoica
+ *
+ * Copyright (c) 2018. Readium Foundation. All rights reserved.
+ * Use of this source code is governed by a BSD-style license which is detailed in the
+ * LICENSE file present in the project repository where this source code is maintained.
+ */
+
+package org.readium.r2.shared
+
+import android.content.Context
+import android.content.ContextWrapper
+
+
+/**
+ * LocatorUtils : API used to get the current location as a Locator object
+ *                  or to reach a Locator place in a publication
+ */
+
+class LocatorUtils {
+
+    // Size of a segment in a publication, expressed in characters
+    val SEGMENT = 1024
+
+    fun getCurrentLocator(){
+
+    }
+    fun goToLocator(){
+
+    }
+
+}
