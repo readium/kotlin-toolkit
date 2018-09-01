0f7c89260716a16b1126c30d1a7c4bf62edfe739
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-09-01 19:15:13 -0400

    commented out, needs to be checked, LocatorUtils is most-likely not needed anymore Test needs update

--- test-app/r2-testapp/src/main/java/org/readium/r2/testapp/LocatorUtils.kt
+++ test-app/r2-testapp/src/main/java/org/readium/r2/testapp/LocatorUtils.kt
@@ -1,137 +1,137 @@
-/*
- * Module: r2-testapp-kotlin
- * Developers: Aferdita Muriqi, Mostapha Idoubihi, Paul Stoica
- *
- * Copyright (c) 2018. Readium Foundation. All rights reserved.
- * Use of this source code is governed by a BSD-style license which is detailed in the
- * LICENSE file present in the project repository where this source code is maintained.
- */
-
-package org.readium.r2.testapp
-
-import android.content.Context
-import org.jetbrains.anko.db.parseList
-import org.jetbrains.anko.db.select
-import org.readium.r2.shared.Locator
-
-
-/**
- * LocatorUtils : API used to get the current location as a Locator object
- *                  or to reach a Locator place in a publication
- */
-
-class LocatorUtils(context: Context) {
-
-    // Size of a segment in a publication, expressed in characters
-    val SEGMENT = 1024
-    var bookmarkDB: BookmarksDatabase = BookmarksDatabase(context)
-
-    fun getBookmarks(bookID: Long? = null): MutableList<Locator> {
-        if (bookID != null) {
-            return bookmarkDB.shared.use {
-                select(BOOKMARKSTable.NAME,
-                        BOOKMARKSTable.ID,
-                        BOOKMARKSTable.BOOK_ID,
-                        BOOKMARKSTable.RESOURCE_INDEX,
-                        BOOKMARKSTable.RESOURCE_HREF,
-                        BOOKMARKSTable.PROGRESSION,
-                        BOOKMARKSTable.CREATED)
-                        .whereArgs("bookID = {bookID}", "bookID" to bookID)
-                        .exec {
-                            parseList(BOOKMARKS.MyRowParser()).toMutableList()
-                        }
-            }
-        } else {
-            return bookmarkDB.shared.use {
-                select(BOOKMARKSTable.NAME,
-                        BOOKMARKSTable.ID,
-                        BOOKMARKSTable.BOOK_ID,
-                        BOOKMARKSTable.RESOURCE_INDEX,
-                        BOOKMARKSTable.RESOURCE_HREF,
-                        BOOKMARKSTable.PROGRESSION,
-                        BOOKMARKSTable.CREATED)
-                        .exec {
-                            parseList(BOOKMARKS.MyRowParser()).toMutableList()
-                        }
-                }
-        }
-    }
-
-    fun getAnnotations(resourceHref: String? = null) {
-
-    }
-
-
-    fun getCurrentLocator(): Locator {
-        return Locator("pid", 1, "href", "title", null)
-    }
-
-    fun goToLocator(locator: Locator) {
-
-    }
-
-
-    fun addLocator(locator: Locator, listOfLocators: MutableList<Locator>): Locator? {
-
-        var addedLocator: Locator? = null
-
-        if (listOfLocators.isEmpty()) {
-            when(locator) {
-                is Bookmark -> {
-                    bookmarkDB.bookmarks.insert(locator)
-                    addedLocator = locator
-                }
-                else -> {
-                    println("An error has occurred while adding the locator")
-                    addedLocator = null
-                }
-            }
-        } else {
-            for (loc in listOfLocators) {
-                if ( loc.location.toString() != locator.location.toString() && loc.location.toString() == listOfLocators.last().location.toString() ) {
-                    when(locator) {
-                        is Bookmark -> {
-                            bookmarkDB.bookmarks.insert(locator)
-                            addedLocator = locator
-                        }
-                        else -> {
-                            println("An error has occurred while adding the locator")
-                            addedLocator = null
-                        }
-                    }
-                } else if (loc.location == locator.location) {
-                    addedLocator = null
-                }
-            }
-        }
-
-        return addedLocator
-    }
-
-
-    fun deleteLocator(locator: Locator, listOfLocators: MutableList<Locator>): Locator? {
-
-        var deletedLocator: Locator? = null
-
-        if( listOfLocators.indexOf(locator) != -1 ) {
-            listOfLocators.remove(locator)
-            when(locator) {
-                is Bookmark -> {
-                    bookmarkDB.bookmarks.delete(locator)
-                    deletedLocator = locator
-                }
-                else -> {
-                    println("An error has occurred while deleting the locator")
-                    deletedLocator = null
-                }
-            }
-        } else {
-            println("Your locator does not exists, and thus, cannot be deleted")
-            deletedLocator = null
-        }
-
-        return deletedLocator
-
-    }
-
-}
+///*
+// * Module: r2-testapp-kotlin
+// * Developers: Aferdita Muriqi, Mostapha Idoubihi, Paul Stoica
+// *
+// * Copyright (c) 2018. Readium Foundation. All rights reserved.
+// * Use of this source code is governed by a BSD-style license which is detailed in the
+// * LICENSE file present in the project repository where this source code is maintained.
+// */
+//
+//package org.readium.r2.testapp
+//
+//import android.content.Context
+//import org.jetbrains.anko.db.parseList
+//import org.jetbrains.anko.db.select
+//import org.readium.r2.shared.Locator
+//
+//
+///**
+// * LocatorUtils : API used to get the current location as a Locator object
+// *                  or to reach a Locator place in a publication
+// */
+//
+//class LocatorUtils(context: Context) {
+//
+//    // Size of a segment in a publication, expressed in characters
+//    val SEGMENT = 1024
+//    var bookmarkDB: BookmarksDatabase = BookmarksDatabase(context)
+//
+//    fun getBookmarks(bookID: Long? = null): MutableList<Locator> {
+//        if (bookID != null) {
+//            return bookmarkDB.shared.use {
+//                select(BOOKMARKSTable.NAME,
+//                        BOOKMARKSTable.ID,
+//                        BOOKMARKSTable.BOOK_ID,
+//                        BOOKMARKSTable.RESOURCE_INDEX,
+//                        BOOKMARKSTable.RESOURCE_HREF,
+//                        BOOKMARKSTable.PROGRESSION,
+//                        BOOKMARKSTable.CREATED)
+//                        .whereArgs("bookID = {bookID}", "bookID" to bookID)
+//                        .exec {
+//                            parseList(BOOKMARKS.MyRowParser()).toMutableList()
+//                        }
+//            }
+//        } else {
+//            return bookmarkDB.shared.use {
+//                select(BOOKMARKSTable.NAME,
+//                        BOOKMARKSTable.ID,
+//                        BOOKMARKSTable.BOOK_ID,
+//                        BOOKMARKSTable.RESOURCE_INDEX,
+//                        BOOKMARKSTable.RESOURCE_HREF,
+//                        BOOKMARKSTable.PROGRESSION,
+//                        BOOKMARKSTable.CREATED)
+//                        .exec {
+//                            parseList(BOOKMARKS.MyRowParser()).toMutableList()
+//                        }
+//                }
+//        }
+//    }
+//
+//    fun getAnnotations(resourceHref: String? = null) {
+//
+//    }
+//
+//
+//    fun getCurrentLocator(): Locator {
+//        return Locator("pid", 1, "href", "title", null)
+//    }
+//
+//    fun goToLocator(locator: Locator) {
+//
+//    }
+//
+//
+//    fun addLocator(locator: Locator, listOfLocators: MutableList<Locator>): Locator? {
+//
+//        var addedLocator: Locator? = null
+//
+//        if (listOfLocators.isEmpty()) {
+//            when(locator) {
+//                is Locator -> {
+//                    bookmarkDB.bookmarks.insert(locator)
+//                    addedLocator = locator
+//                }
+//                else -> {
+//                    println("An error has occurred while adding the locator")
+//                    addedLocator = null
+//                }
+//            }
+//        } else {
+//            for (loc in listOfLocators) {
+//                if ( loc.location.toString() != locator.location.toString() && loc.location.toString() == listOfLocators.last().location.toString() ) {
+//                    when(locator) {
+//                        is Locator -> {
+//                            bookmarkDB.bookmarks.insert(locator)
+//                            addedLocator = locator
+//                        }
+//                        else -> {
+//                            println("An error has occurred while adding the locator")
+//                            addedLocator = null
+//                        }
+//                    }
+//                } else if (loc.location == locator.location) {
+//                    addedLocator = null
+//                }
+//            }
+//        }
+//
+//        return addedLocator
+//    }
+//
+//
+//    fun deleteLocator(locator: Locator, listOfLocators: MutableList<Locator>): Locator? {
+//
+//        var deletedLocator: Locator? = null
+//
+//        if( listOfLocators.indexOf(locator) != -1 ) {
+//            listOfLocators.remove(locator)
+//            when(locator) {
+//                is Locator -> {
+//                    bookmarkDB.bookmarks.delete(locator)
+//                    deletedLocator = locator
+//                }
+//                else -> {
+//                    println("An error has occurred while deleting the locator")
+//                    deletedLocator = null
+//                }
+//            }
+//        } else {
+//            println("Your locator does not exists, and thus, cannot be deleted")
+//            deletedLocator = null
+//        }
+//
+//        return deletedLocator
+//
+//    }
+//
+//}
--- test-app/r2-testapp/src/test/java/org/readium/r2/TestBookmarksDatabase.kt
+++ test-app/r2-testapp/src/test/java/org/readium/r2/TestBookmarksDatabase.kt
@@ -1,108 +1,109 @@
-/*
- * Module: r2-testapp-kotlin
- * Developers: Aferdita Muriqi, Mostapha Idoubihi, Paul Stoica
- *
- * Copyright (c) 2018. European Digital Reading Lab. All rights reserved.
- * Licensed to the Readium Foundation under one or more contributor license agreements.
- * Use of this source code is governed by a BSD-style license which is detailed in the
- * LICENSE file present in the project repository where this source code is maintained.
- */
-
-package org.readium.r2
-
-import android.content.Context
-import org.readium.r2.testapp.Bookmark
-import org.readium.r2.testapp.BookmarksDatabase
-
-/**
- * Unit tests for the BookmarksDatabase helpers with dummies Bookmarks
- */
-class TestBookmarksDatabase(val context: Context) {
-
-    val bmk_db = BookmarksDatabase(context)
-    val bmk_list = mutableListOf<Bookmark>()
-    val wrong_bmk_list = mutableListOf<Bookmark>()
-
-    init {
-        bmk_list.add(Bookmark(1, 1, "dummy",0.0))
-        bmk_list.add(Bookmark(2, 3, "dummy",0.5))
-        bmk_list.add(Bookmark(2, 3, "dummy",0.5))
-        bmk_list.add(Bookmark(15, 12, "dummy",0.9999))
-
-        wrong_bmk_list.add(Bookmark(4, 34, "dummy",1.3333))
-        wrong_bmk_list.add(Bookmark(-4, 34, "dummy",0.3333))
-        wrong_bmk_list.add(Bookmark(4, -34, "dummy",0.3333))
-    }
-
-    fun test(){
-        println("#####################################")
-        println("#########    Bookmarks    ###########")
-        var i = 0
-        bmk_list.forEach {
-            i++
-            println("Book $i : ")
-            println(" - bookId = ${it.bookID} (${it.bookID.javaClass})")
-            try {
-                val ret = bmk_db.bookmarks.insert(it)
-                if (ret != null) {
-                    println("Success : The Bookmark was added in the database ! result : $ret")
-                } else {
-                    println("Failed : The Bookmark couldn't be added, it exist already ! result : $ret")
-                }
-            } catch (e: Exception) {
-                println("Failed : Error while communicating with the database : ${e.message}")
-            }
-
-            try {
-                val ret = bmk_db.bookmarks.has(it)
-                if (ret.isNotEmpty()) {
-                    println("Success : The Bookmark was found in the database ! result : $ret")
-                } else {
-                    println("Failed : The Bookmark not found ! result : $ret")
-                }
-                //                bmk_db.bookmarks.delete(ret.first())
-                //                ret = db.bookmarks.has(it)
-                //                if (ret.isNotEmpty()) { println("Delete failed : $ret !") } else { println("Correctly deleted !") }
-            } catch (e: Exception) {
-                println("Failed : Error while communicating with the database : ${e.message}")
-            }
-        }
-        println("List of BookMarks  : ")
-        bmk_db.bookmarks.listAll().forEach { println(it) }
-        println("-------------------------------------")
-        println("--------  Wrong Bookmarks  ----------")
-        wrong_bmk_list.forEach {
-            i++
-            println("Book $i : ")
-            println(" - bookId = ${it.bookID} (${it.bookID.javaClass})")
-            try {
-                try {
-                    val ret = bmk_db.bookmarks.insert(it)
-                    if (ret != null) {
-                        println("Failed : The Bookmark shouldn't have been added ! result : $ret")
-                    } else {
-                        println("Success : The Bookmark wasn't added in the database ! result : $ret")
-                    }
-                } catch (e: Exception) {
-                    println("Failed : Error while communicating with the database : ${e.message}")
-                }
-                val ret = bmk_db.bookmarks.has(it)
-                if (ret.isNotEmpty()) {
-                    println("Failed : That Bookmark wasn't supposed to be founded ! result : $ret")
-                } else {
-                    println("Success : That Bookmark isn't in the db ! result : $ret")
-                }
-            } catch (e: Exception) {
-                println("Failed : Error while communicating with the database : ${e.message}")
-            }
-        }
-        try {
-            bmk_db.bookmarks.emptyTable()
-            println("Bookmarks cleared with success !")
-        } catch (e: Exception) {
-            println("Failed : Error while communicating with the database : ${e.message}")
-        }
-        println("###########    End     ##############")
-        println("#####################################")
-    }
-}
+///*
+// * Module: r2-testapp-kotlin
+// * Developers: Aferdita Muriqi, Mostapha Idoubihi, Paul Stoica
+// *
+// * Copyright (c) 2018. European Digital Reading Lab. All rights reserved.
+// * Licensed to the Readium Foundation under one or more contributor license agreements.
+// * Use of this source code is governed by a BSD-style license which is detailed in the
+// * LICENSE file present in the project repository where this source code is maintained.
+// */
+//
+//package org.readium.r2
+//
+//import android.content.Context
+//import org.readium.r2.shared.Locator
+//import org.readium.r2.testapp.Locator
+//import org.readium.r2.testapp.BookmarksDatabase
+//
+///**
+// * Unit tests for the BookmarksDatabase helpers with dummies Bookmarks
+// */
+//class TestBookmarksDatabase(val context: Context) {
+//
+//    val bmk_db = BookmarksDatabase(context)
+//    val bmk_list = mutableListOf<Locator>()
+//    val wrong_bmk_list = mutableListOf<Locator>()
+//
+//    init {
+//        bmk_list.add(Locator(1, 1, "dummy",0.0))
+//        bmk_list.add(Locator(2, 3, "dummy",0.5))
+//        bmk_list.add(Locator(2, 3, "dummy",0.5))
+//        bmk_list.add(Locator(15, 12, "dummy",0.9999))
+//
+//        wrong_bmk_list.add(Locator(4, 34, "dummy",1.3333))
+//        wrong_bmk_list.add(Locator(-4, 34, "dummy",0.3333))
+//        wrong_bmk_list.add(Locator(4, -34, "dummy",0.3333))
+//    }
+//
+//    fun test(){
+//        println("#####################################")
+//        println("#########    Bookmarks    ###########")
+//        var i = 0
+//        bmk_list.forEach {
+//            i++
+//            println("Book $i : ")
+//            println(" - bookId = ${it.bookID} (${it.bookID.javaClass})")
+//            try {
+//                val ret = bmk_db.bookmarks.insert(it)
+//                if (ret != null) {
+//                    println("Success : The Locator was added in the database ! result : $ret")
+//                } else {
+//                    println("Failed : The Locator couldn't be added, it exist already ! result : $ret")
+//                }
+//            } catch (e: Exception) {
+//                println("Failed : Error while communicating with the database : ${e.message}")
+//            }
+//
+//            try {
+//                val ret = bmk_db.bookmarks.has(it)
+//                if (ret.isNotEmpty()) {
+//                    println("Success : The Locator was found in the database ! result : $ret")
+//                } else {
+//                    println("Failed : The Locator not found ! result : $ret")
+//                }
+//                //                bmk_db.bookmarks.delete(ret.first())
+//                //                ret = db.bookmarks.has(it)
+//                //                if (ret.isNotEmpty()) { println("Delete failed : $ret !") } else { println("Correctly deleted !") }
+//            } catch (e: Exception) {
+//                println("Failed : Error while communicating with the database : ${e.message}")
+//            }
+//        }
+//        println("List of BookMarks  : ")
+//        bmk_db.bookmarks.listAll().forEach { println(it) }
+//        println("-------------------------------------")
+//        println("--------  Wrong Bookmarks  ----------")
+//        wrong_bmk_list.forEach {
+//            i++
+//            println("Book $i : ")
+//            println(" - bookId = ${it.bookID} (${it.bookID.javaClass})")
+//            try {
+//                try {
+//                    val ret = bmk_db.bookmarks.insert(it)
+//                    if (ret != null) {
+//                        println("Failed : The Locator shouldn't have been added ! result : $ret")
+//                    } else {
+//                        println("Success : The Locator wasn't added in the database ! result : $ret")
+//                    }
+//                } catch (e: Exception) {
+//                    println("Failed : Error while communicating with the database : ${e.message}")
+//                }
+//                val ret = bmk_db.bookmarks.has(it)
+//                if (ret.isNotEmpty()) {
+//                    println("Failed : That Locator wasn't supposed to be founded ! result : $ret")
+//                } else {
+//                    println("Success : That Locator isn't in the db ! result : $ret")
+//                }
+//            } catch (e: Exception) {
+//                println("Failed : Error while communicating with the database : ${e.message}")
+//            }
+//        }
+//        try {
+//            bmk_db.bookmarks.emptyTable()
+//            println("Bookmarks cleared with success !")
+//        } catch (e: Exception) {
+//            println("Failed : Error while communicating with the database : ${e.message}")
+//        }
+//        println("###########    End     ##############")
+//        println("#####################################")
+//    }
+//}
