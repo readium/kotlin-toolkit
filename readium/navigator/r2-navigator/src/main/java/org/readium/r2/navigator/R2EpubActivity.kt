170d3e3fddd29abea41a96bf216b0a7d2c1d1092
Paul Stoica <stoica.paul92@gmail.com>
2018-09-27 14:49:24 +0200

    fixed resources change when in RTL

--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/R2EpubActivity.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/R2EpubActivity.kt
@@ -255,7 +255,7 @@ open class R2EpubActivity : AppCompatActivity() {
             if (ViewCompat.getLayoutDirection(this.contentView) == ViewCompat.LAYOUT_DIRECTION_RTL || publication.metadata.direction == PageProgressionDirection.rtl.name) {
                 // The view has RTL layout
                 resourcePager.webView.progression = 1.0
-                resourcePager.currentItem = resourcePager.currentItem - 1
+                resourcePager.currentItem = resourcePager.currentItem + 1
 
             } else {
                 // The view has LTR layout
@@ -274,7 +274,7 @@ open class R2EpubActivity : AppCompatActivity() {
             if (ViewCompat.getLayoutDirection(this.contentView) == ViewCompat.LAYOUT_DIRECTION_RTL || publication.metadata.direction == PageProgressionDirection.rtl.name) {
                 // The view has RTL layout
                 resourcePager.webView.progression = 0.0
-                resourcePager.currentItem = resourcePager.currentItem + 1
+                resourcePager.currentItem = resourcePager.currentItem - 1
             } else {
                 // The view has LTR layout
                 resourcePager.webView.progression = 1.0
