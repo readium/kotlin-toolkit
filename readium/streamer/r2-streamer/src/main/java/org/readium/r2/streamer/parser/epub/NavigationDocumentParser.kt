ecf4848327865b87aa5e3ab810976527bf4f8470
Paul Stoica <stoica.paul92@gmail.com>
2018-11-06 16:03:30 +0100

    added toc parsing with xpath

--- readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/epub/NavigationDocumentParser.kt
+++ readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/epub/NavigationDocumentParser.kt
@@ -13,12 +13,37 @@ import org.readium.r2.shared.Link
 import org.readium.r2.shared.parser.xml.XmlParser
 import org.readium.r2.shared.parser.xml.Node
 import org.readium.r2.streamer.parser.normalize
+import org.w3c.dom.NodeList
+import java.io.InputStream
+import javax.xml.XMLConstants
+import javax.xml.namespace.NamespaceContext
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathFactory
 
 class NavigationDocumentParser {
 
     var navigationDocumentPath: String = ""
 
-    fun tableOfContent(document: XmlParser) = nodeArray(document, "toc")
+    fun tableOfContent(xml: ByteArray) : MutableList<Link> {
+        val tableOfContents = mutableListOf<Link>()
+        val document = xml.inputStream()
+
+        val xpath = "/xhtml:html/xhtml:body/xhtml:nav[@epub:type='toc']//xhtml:a" + "|/xhtml:html/xhtml:body/xhtml:nav[@epub:type='toc']//xhtml:span"
+        val nodes = evaluateXpath(xpath, document)
+
+        for (i in 0 until nodes.length) {
+            val link = Link()
+
+            link.href = nodes.item(i).attributes.getNamedItem("href")?.nodeValue
+            link.title = nodes.item(i).textContent
+
+            tableOfContents.add(link)
+        }
+
+        return tableOfContents
+    }
+
     fun pageList(document: XmlParser) = nodeArray(document, "page-list")
     fun landmarks(document: XmlParser) = nodeArray(document, "landmarks")
     fun listOfIllustrations(document: XmlParser) = nodeArray(document, "loi")
@@ -61,4 +86,39 @@ class NavigationDocumentParser {
         return newLiNode
     }
 
+
+    private fun evaluateXpath(expression: String, doc: InputStream): NodeList {
+
+        val dbFactory = DocumentBuilderFactory.newInstance()
+        dbFactory.isNamespaceAware = true
+        val docBuilder  = dbFactory.newDocumentBuilder()
+
+        val document = docBuilder.parse(doc)
+
+        val xPath = XPathFactory.newInstance().newXPath()
+        xPath.namespaceContext = NameSpaceResolver()
+
+        return xPath.evaluate(expression, document, XPathConstants.NODESET) as NodeList
+    }
+
 }
+
+class NameSpaceResolver : NamespaceContext {
+    override fun getNamespaceURI(p0: String?): String {
+        when (p0) {
+            null -> throw IllegalArgumentException("No prefix provided!")
+            "epub" -> return "http://www.idpf.org/2007/ops"
+            "xhtml" -> return "http://www.w3.org/1999/xhtml"
+            else -> return XMLConstants.DEFAULT_NS_PREFIX
+        }
+    }
+
+    override fun getPrefix(p0: String?): String? {
+        return null
+    }
+
+    override fun getPrefixes(p0: String?): MutableIterator<Any?>? {
+        return null
+    }
+}
+
