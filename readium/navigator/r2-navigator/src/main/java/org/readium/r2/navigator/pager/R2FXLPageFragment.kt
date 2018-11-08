9cf48cc5b77c7c3430c9a15192792142e2f81f0c
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-11-07 19:59:00 -0500

    Revert "cleaned up FXL page fragment"
    This reverts commit ad48d3160950aefef43dc8e431307738a76ccf10.

--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2FXLPageFragment.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2FXLPageFragment.kt
@@ -11,6 +11,7 @@ package org.readium.r2.navigator.pager
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -20,10 +21,12 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import org.json.JSONObject
 import org.readium.r2.navigator.R
 import org.readium.r2.navigator.R2EpubActivity
-import org.readium.r2.navigator.fxl.ZoomLayout
-import org.readium.r2.navigator.fxl.ZoomOnDoubleTapListener
+import org.readium.r2.navigator.fxl.R2FXLLayout
+import org.readium.r2.navigator.fxl.R2FXLOnDoubleTapListener
+import org.readium.r2.shared.Locations
 
 
 class R2FXLPageFragment : Fragment() {
@@ -41,54 +44,32 @@ class R2FXLPageFragment : Fragment() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        secondResourceUrl?.let {
-            val view: View = inflater.inflate(R.layout.fxlview_double, container, false)
-            view.setPadding(0, 0, 0, 0)
+        val preferences = activity?.getSharedPreferences("org.readium.r2.settings", Context.MODE_PRIVATE)!!
 
-            val r2FXLLayout = view.findViewById<View>(R.id.r2FXLLayout) as ZoomLayout
-            r2FXLLayout.isAllowParentInterceptOnScaled = true
-            r2FXLLayout.addOnDoubleTapListener(ZoomOnDoubleTapListener(true))
+        val view: View = inflater.inflate(R.layout.fxlview_double, container, false)
 
-            r2FXLLayout.addOnTapListener(object : ZoomLayout.OnTapListener {
-                override fun onTap(view: ZoomLayout, info: ZoomLayout.TapInfo): Boolean {
-                    (activity as R2EpubActivity).toggleActionBar()
-                    return true
-                }
-            })
+        val r2FXLLayout = view.findViewById<View>(R.id.r2FXLLayout) as R2FXLLayout
+        r2FXLLayout.isAllowParentInterceptOnScaled = true
+        r2FXLLayout.addOnDoubleTapListener(R2FXLOnDoubleTapListener(true))
 
-            val left = view.findViewById<View>(R.id.firstWebView) as R2BasicWebView
-            val right = view.findViewById<View>(R.id.secondWebView) as R2BasicWebView
+        r2FXLLayout.addOnTapListener(object : R2FXLLayout.OnTapListener {
+            override fun onTap(view: R2FXLLayout, info: R2FXLLayout.TapInfo): Boolean {
+                (activity as R2EpubActivity).toggleActionBar()
+                return true
+            }
+        })
 
-            setupWebView(left, firstResourceUrl)
-            setupWebView(right, secondResourceUrl)
+        val left = view.findViewById<View>(R.id.firstWebView) as R2WebView
+        val right = view.findViewById<View>(R.id.secondWebView) as R2WebView
 
-            return view
-        }?:run {
-            val view: View = inflater.inflate(R.layout.fxlview_single, container, false)
-            view.setPadding(0, 0, 0, 0)
+        setupWebView(left, preferences, firstResourceUrl, r2FXLLayout)
+        setupWebView(right, preferences, secondResourceUrl, r2FXLLayout)
 
-            val r2FXLLayout = view.findViewById<View>(R.id.r2FXLLayout) as ZoomLayout
-            r2FXLLayout.isAllowParentInterceptOnScaled = true
-            r2FXLLayout.isScrollingAllowed = true
-            r2FXLLayout.addOnDoubleTapListener(ZoomOnDoubleTapListener(true))
-
-            r2FXLLayout.addOnTapListener(object : ZoomLayout.OnTapListener {
-                override fun onTap(view: ZoomLayout, info: ZoomLayout.TapInfo): Boolean {
-                    (activity as R2EpubActivity).toggleActionBar()
-                    return true
-                }
-            })
-
-            val webview = view.findViewById<View>(R.id.webViewSingle) as R2BasicWebView
-
-            setupWebView(webview, firstResourceUrl)
-
-            return view
-        }
+        return view
     }
 
     @SuppressLint("SetJavaScriptEnabled")
-    private fun setupWebView(webView: R2BasicWebView, resourceUrl: String?) {
+    private fun setupWebView(webView: R2WebView, preferences: SharedPreferences, resourceUrl: String?, r2FXLLayout: R2FXLLayout) {
         webView.activity = activity as R2EpubActivity
 
         webView.settings.javaScriptEnabled = true
@@ -96,20 +77,70 @@ class R2FXLPageFragment : Fragment() {
         webView.isHorizontalScrollBarEnabled = false
         webView.settings.useWideViewPort = true
         webView.settings.loadWithOverviewMode = true
-        webView.settings.setSupportZoom(true)
-        webView.settings.builtInZoomControls = true
-        webView.settings.displayZoomControls = false
 
         webView.setInitialScale(1)
 
-        webView.setPadding(0, 0, 0, 0)
         webView.addJavascriptInterface(webView, "Android")
-
 
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
                 view.loadUrl(request.url.toString())
                 return false
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                try {
+                    val childCount = webView.activity.resourcePager.childCount
+
+                    if (webView.activity.reloadPagerPositions) {
+                        if (childCount == 2) {
+                            when {
+                                webView.activity.pagerPosition == 0 -> {
+                                    val locations = Locations.fromJSON(JSONObject(preferences.getString("${webView.activity.publicationIdentifier}-documentLocations", "{}")))
+                                    locations.progression?.let { progression ->
+                                        webView.scrollToPosition(progression)
+                                    }
+                                    webView.activity.pagerPosition++
+                                }
+                                else -> {
+                                    webView.scrollToPosition(0.0)
+                                    webView.activity.pagerPosition = 0
+                                    webView.activity.reloadPagerPositions = false
+                                }
+                            }
+                        } else {
+                            when {
+                                webView.activity.pagerPosition == 0 -> {
+                                    val locations = Locations.fromJSON(JSONObject(preferences.getString("${webView.activity.publicationIdentifier}-documentLocations", "{}")))
+                                    locations.progression?.let { progression ->
+                                        webView.scrollToPosition(progression)
+                                    }
+                                    webView.activity.pagerPosition++
+                                }
+                                webView.activity.pagerPosition == 1 -> {
+                                    webView.scrollToPosition(1.0)
+                                    webView.activity.pagerPosition++
+                                }
+                                else -> {
+                                    webView.scrollToPosition(0.0)
+                                    webView.activity.pagerPosition = 0
+                                    webView.activity.reloadPagerPositions = false
+                                }
+                            }
+                        }
+                    } else {
+                        webView.activity.pagerPosition = 0
+                        val locations = Locations.fromJSON(JSONObject(preferences.getString("${webView.activity.publicationIdentifier}-documentLocations", "{}")))
+                        locations.progression?.let { progression ->
+                            webView.scrollToPosition(progression)
+                        }
+                    }
+                } catch (e: Exception) {
+                    // TODO double check this error, a crash happens when scrolling to fast between resources.....
+                    // kotlin.TypeCastException: null cannot be cast to non-null type org.readium.r2.navigator.R2EpubActivity
+                }
+
             }
 
             // prevent favicon.ico to be loaded, this was causing NullPointerException in NanoHttp
@@ -129,13 +160,37 @@ class R2FXLPageFragment : Fragment() {
         webView.setOnLongClickListener {
             true
         }
-        
+//        webView.setGestureDetector(GestureDetector(context, CustomGestureDetector(webView)))
         webView.loadUrl(resourceUrl)
     }
 
+//    class CustomGestureDetector(val webView: R2WebView) : GestureDetector.SimpleOnGestureListener() {
+//
+//        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+//            if (e1 == null || e2 == null) return false
+//            if (e1.pointerCount > 1 || e2.pointerCount > 1)
+//                return false
+//            else {
+//                try { // right to left swipe .. go to next page
+//                    if (e1.x - e2.x > 100) {
+//                        webView.scrollRight()
+//                        return true
+//                    } //left to right swipe .. go to prev page
+//                    else if (e2.x - e1.x > 100) {
+//                        webView.scrollLeft()
+//                        return true
+//                    }
+//                } catch (e: Exception) { // nothing
+//                }
+//
+//                return false
+//            }
+//        }
+//    }
+
     companion object {
 
-        fun newInstance(title: String, url: String, url2: String? = null): R2FXLPageFragment {
+        fun newInstance(url: String, url2: String, title: String): R2FXLPageFragment {
 
             val args = Bundle()
             args.putString("firstUrl", url)
--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2PagerAdapter.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2PagerAdapter.kt
@@ -51,11 +51,11 @@ class R2PagerAdapter(fm: FragmentManager, private val resources: List<Any>, priv
                 Publication.TYPE.FXL -> {
                     if (resources[position] is Triple<*, *, *>) {
                         val double = resources[position] as Triple<Int, String, String>
-                        R2FXLPageFragment.newInstance(title, double.second, double.third)
+                        R2FXLPageFragment.newInstance(double.second, double.third, title)
                     }
                     else {
                         val single = resources[position] as Pair<Int, String>
-                        R2FXLPageFragment.newInstance(title, single.second)
+                        R2EpubPageFragment.newInstance(single.second, title)
                     }
                 }
                 Publication.TYPE.CBZ -> R2CbzPageFragment.newInstance(publicationPath, resources[position] as String)
