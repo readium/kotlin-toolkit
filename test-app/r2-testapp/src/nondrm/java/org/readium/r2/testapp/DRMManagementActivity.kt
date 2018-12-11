f2e77a7e3bfd7362768da6ddcd9bf25ca3dfa81f
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-12-11 11:15:45 +0100

    moved DRM activity to LCP flavor

---
+++ test-app/r2-testapp/src/nondrm/java/org/readium/r2/testapp/DRMManagementActivity.kt
@@ -0,0 +1,18 @@
+/*
+ * Module: r2-testapp-kotlin
+ * Developers: Aferdita Muriqi, Clément Baumann
+ *
+ * Copyright (c) 2018. European Digital Reading Lab. All rights reserved.
+ * Licensed to the Readium Foundation under one or more contributor license agreements.
+ * Use of this source code is governed by a BSD-style license which is detailed in the
+ * LICENSE file present in the project repository where this source code is maintained.
+ */
+
+package org.readium.r2.testapp
+
+import android.support.v7.app.AppCompatActivity
+
+
+class DRMManagementActivity : AppCompatActivity() {
+
+}
