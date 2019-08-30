03c1002c2396cd6b046e3ad04f746223aaff2b26
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-08-30 17:20:17 -0400

    minor modifications on the search interface and added a completion callback,

--- test-app/r2-testapp/src/main/java/org/readium/r2/testapp/search/SearchInterface.kt
+++ test-app/r2-testapp/src/main/java/org/readium/r2/testapp/search/SearchInterface.kt
@@ -1,12 +1,11 @@
 package org.readium.r2.testapp.search
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import org.json.JSONArray
 import org.jsoup.Jsoup
 import org.readium.r2.navigator.BASE_URL
@@ -19,34 +18,39 @@ import org.readium.r2.shared.Publication
  *
  */
 interface SearchInterface {
-    fun search(keyword: String, context: Context) : List<SearchLocator>
+    fun search(keyword: String, context: Context, callback: (Pair<Boolean, MutableList<SearchLocator>>) -> Unit)
+}
+
+interface R2Search {
+    fun injectMarkJS()
+    fun injectScriptFile(view: WebView, scriptFile: String)
 }
 
 /**
  * This is our custom Search Module, this class uses MarkJS library and implements SearchInterface
  */
-class MyMarkJSSearchInteface(var publication: Publication, var publicationIdentifier: String, var preferences: SharedPreferences, var epubName: String, var bv: BooVariable) : SearchInterface {
+class MarkJSSearchInterface(var publication: Publication, private var publicationIdentifier: String, private var preferences: SharedPreferences, var epubName: String) : SearchInterface {
 
     /**
      * This function is used to get search results using JSOUP and MarkJS
      * This function build a temporary WebView and executes JS code on it
      */
-    override fun search(keyword: String, context: Context): List<SearchLocator> {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun search(keyword: String, context: Context, callback: (Pair<Boolean, MutableList<SearchLocator>>) -> Unit) {
         //Setting up webView in order to execute JS code
-        var webView = WebView(context)
+        val webView = WebView(context)
         webView.webChromeClient = WebChromeClient()
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         webView.settings.javaScriptEnabled = true
 
         //Loading empty HTML with JS files linked
-        var url = "file:///android_asset/index.html"
+        val url = "file:///android_asset/Search/index.html"
         webView.loadUrl(url)
 
         //Setting up variables for iteration
-        val port = preferences.getString("$publicationIdentifier-publicationPort", 0.toString()).toInt()
-        var title = publication.metadata.title
+        val port = preferences.getString("$publicationIdentifier-publicationPort", 0.toString())?.toInt()
         var resourceNumber = 0
-        var locatorsList = mutableListOf<SearchLocator>()
+        val locatorsList = mutableListOf<SearchLocator>()
 
         //When webview is loaded -> execute JS to get results
         webView.webViewClient = object : WebViewClient() {
@@ -54,7 +58,7 @@ class MyMarkJSSearchInteface(var publication: Publication, var publicationIdenti
                 super.onPageFinished(view, url)
                 //Iterating through resources and executing markJS
                 while (resourceNumber < publication.readingOrder.size) {
-                    lateinit var resource : org.jsoup.nodes.Document
+                    lateinit var resource: org.jsoup.nodes.Document
 
                     //Creating thread to get resource's HTML content as String using JSOUP
                     val thread = Thread(Runnable {
@@ -67,8 +71,12 @@ class MyMarkJSSearchInteface(var publication: Publication, var publicationIdenti
 
                     //When thread ends, document should be non null
                     //For each resource, find results using MarkJS
-                    resource?.let {
-                        var resourceId = publication.readingOrder[resourceNumber].href as String
+                    resource.let {
+                        val resource1 = publication.readingOrder[resourceNumber]
+                        val resourceHref = resource1.href ?: ""
+                        val resourceType = resource1.typeLink ?: ""
+                        val resourceTitle = resource1.title ?: ""
+
                         //Element is current HTML resource as String
                         var element = resource.getElementsByClass("body").text()
 
@@ -78,33 +86,33 @@ class MyMarkJSSearchInteface(var publication: Publication, var publicationIdenti
 
 
                         //Executing MarkJS to get results
-                        webView.evaluateJavascript("performSearch(\"$keyword\", \"$element\", \"$resourceId\");") { results ->
-                            if(results != "null") {
-                                //Transforming json string
-                                var json = results
-                                json = json.replace("\\\"", "'")
-
-                                val jsonLocators = JSONArray(json.substring(1, json.length - 1))
-                                for (i in 0..(jsonLocators.length() - 1)) {
+                        webView.evaluateJavascript("markSearch(\"$keyword\", \"$element\", \"$resourceHref\", \"$resourceType\", \"$resourceTitle\")") { results ->
+                            val locators = JSONArray(results)
+                            if (results.isNotEmpty()) {
+                                for (i in 0 until locators.length()) {
                                     //Building Locators Objects
-                                    val resultObj = jsonLocators.getJSONObject(i)
-                                    var href = resultObj.getString("href")
-                                    var type = resultObj.getString("type")
-                                    var text = LocatorText.fromJSON(resultObj.getJSONObject("text"))
-                                    var location = Locations.fromJSON(resultObj.getJSONObject("location"))
-                                    var tmpLocator = SearchLocator(href, type, title ,location, text)
+                                    val locator = locators.getJSONObject(i)
+                                    val href = locator.getString("href")
+                                    val type = locator.getString("type")
+                                    val title = locator.getString("title")
+                                    val text = LocatorText.fromJSON(locator.getJSONObject("text"))
+                                    val location = Locations.fromJSON(locator.getJSONObject("locations"))
+                                    val tmpLocator = SearchLocator(href, type, title, location, text)
                                     locatorsList.add(tmpLocator)
-                                    bv.resultsList = locatorsList
                                 }
+                                callback(Pair(false, locatorsList))
                             }
                         }
 
                     }
                     resourceNumber++
                 }
+                if (resourceNumber == publication.readingOrder.size) {
+                    callback(Pair(true, locatorsList))
+                }
             }
         }
-        //Returning this empty list because we already use "bv" variable to trigger our R2SearchActivity that JS is fully executed
-        return mutableListOf()
+
     }
+
 }
