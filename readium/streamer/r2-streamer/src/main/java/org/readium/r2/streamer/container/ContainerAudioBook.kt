973a36eda678c0ef163c6f285c594308bf516e86
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-09-04 08:27:45 -0400

    added missing copyright header

--- readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/container/ContainerAudioBook.kt
+++ readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/container/ContainerAudioBook.kt
@@ -1,19 +1,18 @@
-package org.readium.r2.streamer.container
-
-import org.readium.r2.shared.RootFile
-import org.readium.r2.shared.drm.DRM
-import org.readium.r2.streamer.parser.AudioBookParser
-import java.io.File
-
 /*
  * Module: r2-streamer-kotlin
- * Developers: Aferdita Muriqi
+ * Developers: Aferdita Muriqi, Irteza Sheikh
  *
  * Copyright (c) 2019. Readium Foundation. All rights reserved.
  * Use of this source code is governed by a BSD-style license which is detailed in the
  * LICENSE file present in the project repository where this source code is maintained.
  */
 
+package org.readium.r2.streamer.container
+
+import org.readium.r2.shared.RootFile
+import org.readium.r2.shared.drm.DRM
+import org.readium.r2.streamer.parser.AudioBookParser
+import java.io.File
 
 class ContainerAudioBook : AudioBookContainer, DirectoryContainer {
 
--- readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/AudioBookParser.kt
+++ readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/AudioBookParser.kt
@@ -1,3 +1,12 @@
+/*
+ * Module: r2-streamer-kotlin
+ * Developers: Aferdita Muriqi, Irteza Sheikh
+ *
+ * Copyright (c) 2018. Readium Foundation. All rights reserved.
+ * Use of this source code is governed by a BSD-style license which is detailed in the
+ * LICENSE file present in the project repository where this source code is maintained.
+ */
+
 package org.readium.r2.streamer.parser
 
 import org.json.JSONObject
