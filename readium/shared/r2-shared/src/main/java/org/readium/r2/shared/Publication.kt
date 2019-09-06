b5da9deebbb7503106a5258257a2562679b41d90
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-09-06 07:28:58 -0400

    use divina extension

--- readium/shared/r2-shared/src/main/java/org/readium/r2/shared/Publication.kt
+++ readium/shared/r2-shared/src/main/java/org/readium/r2/shared/Publication.kt
@@ -95,7 +95,7 @@ class Publication : Serializable {
         EPUB(".epub"),
         CBZ(".cbz"),
         JSON(".json"),
-        ZIP(".zip"),
+        DIVINA(".divina"),
         AUDIO(".audiobook");
 
         companion object : EnumCompanion<String, EXTENSION>(values().associateBy(EXTENSION::value))
