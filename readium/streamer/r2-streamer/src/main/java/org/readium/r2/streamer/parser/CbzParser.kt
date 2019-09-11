3aa0032cead8fd6e954fc08f17a3d7aac1b18fb8
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-09-11 11:28:31 -0400

    optimized cbz parser

--- readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/CbzParser.kt
+++ readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/CbzParser.kt
@@ -9,12 +9,20 @@
 
 package org.readium.r2.streamer.parser
 
+import android.net.Uri
+import android.os.Build
+import android.text.TextUtils
 import android.webkit.MimeTypeMap
 import org.readium.r2.shared.Link
 import org.readium.r2.shared.Publication
 import org.readium.r2.streamer.container.ContainerCbz
 import timber.log.Timber
 import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.nio.file.Paths
+import java.security.MessageDigest
+import kotlin.experimental.and
 
 
 /**
@@ -98,30 +106,87 @@ class CbzParser : PublicationParser {
             link.href = it
 
             if (getMimeType(it) == mimetypeJPEG || getMimeType(it) == mimetypePNG) {
-                publication.pageList.add(link)
+                publication.readingOrder.add(link)
             } else {
-                publication.resources.add(link)         //List of eventual extra files ( .nfo, ect .. )
+                publication.resources.add(link)
             }
         }
-        publication.pageList.first().rel.add("cover")
-        publication.metadata.identifier = fileAtPath
-//        publication.metadata.multilanguageTitle = MultilanguageString()
-//        publication.metadata.multilanguageTitle?.singleString = container.title
-//        publication.metadata.title = container.getTitle()
+        val hash = fileToMD5(fileAtPath)
+        publication.readingOrder.first().rel.add("cover")
+
+        // Add href as title if title is missing (this is used to display the TOC)
+        for ((index, link) in publication.readingOrder.withIndex()) {
+            if (link.title == null || link.title!!.isEmpty()) {
+                link.title = link.href
+            }
+        }
+
+        publication.readingOrder = publication.readingOrder.sortedWith(compareBy {it.href}).toMutableList()
+
+        publication.metadata.identifier = hash!!
         publication.type = Publication.TYPE.CBZ
         return PubBox(publication, container)
     }
 
-    private fun getMimeType(fileName: String): String? {
+    private fun getMimeType(file: String): String? {
         return try {
-            val name = fileName.replace(" ", "").replace("'", "").replace(",", "")
+            val lastSegment = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val path = Paths.get(file)
+                path.fileName.toString()
+            } else {
+                val uri = Uri.parse(file);
+                uri.lastPathSegment
+            }
+            var type: String? = null
+            val name = lastSegment?.replace(" ", "")?.replace("'", "")?.replace(",", "")
             val extension = MimeTypeMap.getFileExtensionFromUrl(name)
-            MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+            if (!TextUtils.isEmpty(extension)) {
+                type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)!!
+            } else {
+                val reCheckExtension = MimeTypeMap.getFileExtensionFromUrl(name?.replace("\\s+", ""))
+                if (!TextUtils.isEmpty(reCheckExtension)) {
+                    type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(reCheckExtension)!!
+                }
+            }
+            type
         } catch (e: Exception) {
             Timber.e(e)
             null
         }
     }
 
+    private fun fileToMD5(filePath: String): String? {
+        var inputStream: InputStream? = null
+        try {
+            inputStream = FileInputStream(filePath)
+            val buffer = ByteArray(1024)
+            val digest = MessageDigest.getInstance("MD5")
+            var numRead = 0
+            while (numRead != -1) {
+                numRead = inputStream.read(buffer)
+                if (numRead > 0)
+                    digest.update(buffer, 0, numRead)
+            }
+            val md5Bytes = digest.digest()
+            return convertHashToString(md5Bytes)
+        } catch (e: Exception) {
+            return null
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close()
+                } catch (e: Exception) {
+                }
 
+            }
+        }
+    }
+
+    private fun convertHashToString(md5Bytes: ByteArray): String {
+        var returnVal = ""
+        for (i in md5Bytes.indices) {
+            returnVal += Integer.toString((md5Bytes[i] and 0xff.toByte()) + 0x100, 16).substring(1)
+        }
+        return returnVal
+    }
 }
