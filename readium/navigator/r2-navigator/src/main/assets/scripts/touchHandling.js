882ef720d3a27a9a2c393ea74c40e9882774f96f
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-11-13 10:33:35 +0100

    fixed an issue on swipe / tap where the tap would get stuck on one page

--- readium/navigator/r2-navigator/src/main/assets/scripts/touchHandling.js
+++ readium/navigator/r2-navigator/src/main/assets/scripts/touchHandling.js
@@ -5,14 +5,15 @@ var availWidth = window.screen.availWidth;
 var availHeight = window.screen.availHeight;
 
 
-window.addEventListener("load", function(){ // on page load
-                        // Get screen X and Y sizes.
-                        // Events listeners for the touches.
-                        window.document.addEventListener("touchstart", handleTouchStart, false);
-                        window.document.addEventListener("touchend", handleTouchEnd, false);
-                        window.document.addEventListener("click",handleClick,true);
-                        // When device orientation changes, screen X and Y sizes are recalculated.
-                        }, false);
+
+window.addEventListener("load", function() { // on page load
+    // Get screen X and Y sizes.
+    // Events listeners for the touches.
+    window.document.addEventListener("touchstart", handleTouchStart, false);
+    window.document.addEventListener("touchend", handleTouchEnd, false);
+    window.document.addEventListener("click", handleClick, true);
+    // When device orientation changes, screen X and Y sizes are recalculated.
+}, false);
 
 
 var handleClick = function(event) {
@@ -39,7 +40,7 @@ var handleTouchStart = function(event) {
 
 // When a touch ends, check if any action has to be made, and contact native code.
 var handleTouchEnd = function(event) {
-    if(!singleTouchGesture) {
+    if (!singleTouchGesture) {
         return;
     }
 
@@ -53,12 +54,12 @@ var handleTouchEnd = function(event) {
     var screenWidth = availWidth;
     var tapAreaWidth = availWidth * 0.2;
 
-    if(touchDistance < 0.01) {
+    if (touchDistance < 0.01) {
         var position = (touch.screenX % availWidth) / availWidth;
         if (position <= 0.2) {
             console.log("LeftTapped");
             Android.scrollLeft();
-        } else if(position >= 0.8) {
+        } else if (position >= 0.8) {
             console.log("RightTapped");
             Android.scrollRight();
         } else {
