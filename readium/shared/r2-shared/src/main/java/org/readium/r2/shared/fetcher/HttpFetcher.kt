33147e3acce50ca80acd07d64e508459dd3e2820
Quentin Gliosca <32197639+qnga@users.noreply.github.com>
2020-04-05 18:06:20 +0200

    Minimally fix HttpFetcher

--- readium/shared/r2-shared/src/main/java/org/readium/r2/shared/fetcher/HttpFetcher.kt
+++ readium/shared/r2-shared/src/main/java/org/readium/r2/shared/fetcher/HttpFetcher.kt
@@ -15,49 +15,41 @@ import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
-
-/**
- * This class implements a lazy fetcher of resources over HTTP.
- *
- * No request is done until a Resource property is called.
- * When metadata is requested before content, a HEAD request is done.
- * Otherwise, the result from the previous GET request is used.
-*/
 class HttpFetcher : Fetcher {
 
     override fun get(link: Link): Resource = HttpResource(link)
 
     override fun close() {}
 
-    private class HttpResource(link: Link): ResourceImpl() {
+    private class HttpResource(link: Link): StreamResource() {
 
-        private val headConnection: HttpURLConnection? by lazy {
-            (URL(link.href).openConnection() as? HttpURLConnection)?.apply { requestMethod = "HEAD" }
+        private val headConnection: HttpURLConnection by lazy {
+            (URL(link.href).openConnection() as HttpURLConnection).apply { requestMethod = "HEAD" }
         }
 
-        private val getConnection: HttpURLConnection? by lazy {
-            (URL(link.href).openConnection() as? HttpURLConnection)?.apply { requestMethod = "GET" }
+        private val getConnection: HttpURLConnection by lazy {
+            (URL(link.href).openConnection() as HttpURLConnection).apply { requestMethod = "GET" }
         }
 
         private val headerFields: Map<String, List<String>>?
             get() = if (::getConnection.isLazyInitialized) {
-                getConnection?.headerFields
+                getConnection.headerFields
             } else {
-                headConnection?.headerFields
+                headConnection.headerFields
             }
+
+        private val acceptsByteRanges by lazy {
+            headerFields?.get("Accept-Ranges")?.firstOrNull() == "bytes"
+        }
 
         override val link: Link by lazy {
             link.copy(type = headerFields?.get("Content-Type")?.firstOrNull { it != "application/octet-stream" } ?: link.type)
         }
 
-        override fun stream(): InputStream? = bytes?.inputStream()  // getConnection?.inputStream always returns the same stream
+        override fun stream(): InputStream = bytes.inputStream()  // getConnection?.inputStream always returns the same stream
 
-        override val bytes: ByteArray? by lazy {
-            try {
-                getConnection?.inputStream?.use { it.readBytes() }
-            } catch (e: Exception) {
-                null
-            }
+        private val bytes: ByteArray by lazy {
+            getConnection.inputStream.use { it.readBytes() }
         }
 
         override val metadataLength: Long? by lazy {
