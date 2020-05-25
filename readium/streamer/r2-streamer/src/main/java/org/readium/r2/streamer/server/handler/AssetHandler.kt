e5701da83d66fb22293009b7349a510c0d9af7b9
Mickaël Menu <mickael.menu@gmail.com>
2020-05-25 15:05:29 +0200

    Refactor HTTP handlers to make them more generic Simplify serving files from the application's assets

--- readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/server/handler/AssetHandler.kt
+++ readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/server/handler/AssetHandler.kt
@@ -1,8 +1,8 @@
 /*
  * Module: r2-streamer-kotlin
- * Developers: Aferdita Muriqi, Clément Baumann, Mickaël Menu
+ * Developers: Mickaël Menu
  *
- * Copyright (c) 2018. Readium Foundation. All rights reserved.
+ * Copyright (c) 2020. Readium Foundation. All rights reserved.
  * Use of this source code is governed by a BSD-style license which is detailed in the
  * LICENSE file present in the project repository where this source code is maintained.
  */
@@ -12,14 +12,19 @@ package org.readium.r2.streamer.server.handler
 import android.net.Uri
 import org.nanohttpd.protocols.http.response.Response
 import org.nanohttpd.router.RouterNanoHTTPD
-import org.readium.r2.shared.format.MediaType
-import org.readium.r2.streamer.fetcher.Fetcher
+import org.readium.r2.streamer.server.Assets
 
-internal class ManifestHandler : BaseHandler() {
+/**
+ * Serves files from the local file system.
+ *
+ * The NanoHTTPD init parameter must be an instance of `Files`.
+ */
+internal class AssetHandler : BaseHandler() {
 
     override fun handle(resource: RouterNanoHTTPD.UriResource, uri: Uri, parameters: Map<String, String>?): Response {
-        val fetcher = resource.initParameter(Fetcher::class.java)
-        return createResponse(mediaType = MediaType.WEBPUB_MANIFEST, body = fetcher.publication.manifest.toByteArray())
+        val assets = resource.initParameter(Assets::class.java)
+        val asset = assets.find(uri) ?: return notFoundResponse
+        return createResponse(mediaType = asset.mediaType, body = asset.stream)
     }
 
 }
--- readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/server/handler/FileHandler.kt
+++ readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/server/handler/FileHandler.kt
@@ -1,8 +1,8 @@
 /*
  * Module: r2-streamer-kotlin
- * Developers: Aferdita Muriqi, Clément Baumann, Mickaël Menu
+ * Developers: Mickaël Menu
  *
- * Copyright (c) 2018. Readium Foundation. All rights reserved.
+ * Copyright (c) 2020. Readium Foundation. All rights reserved.
  * Use of this source code is governed by a BSD-style license which is detailed in the
  * LICENSE file present in the project repository where this source code is maintained.
  */
@@ -12,14 +12,19 @@ package org.readium.r2.streamer.server.handler
 import android.net.Uri
 import org.nanohttpd.protocols.http.response.Response
 import org.nanohttpd.router.RouterNanoHTTPD
-import org.readium.r2.shared.format.MediaType
-import org.readium.r2.streamer.fetcher.Fetcher
+import org.readium.r2.streamer.server.Files
 
-internal class ManifestHandler : BaseHandler() {
+/**
+ * Serves files from the local file system.
+ *
+ * The NanoHTTPD init parameter must be an instance of `Files`.
+ */
+internal class FileHandler : BaseHandler() {
 
     override fun handle(resource: RouterNanoHTTPD.UriResource, uri: Uri, parameters: Map<String, String>?): Response {
-        val fetcher = resource.initParameter(Fetcher::class.java)
-        return createResponse(mediaType = MediaType.WEBPUB_MANIFEST, body = fetcher.publication.manifest.toByteArray())
+        val files = resource.initParameter(Files::class.java)
+        val file = files.find(uri) ?: return notFoundResponse
+        return createResponse(mediaType = file.mediaType, body = file.file.readBytes())
     }
 
 }
