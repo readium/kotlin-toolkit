37ce0417e1e94ecd480966b49b4e3554d9368cfe
Quentin Gliosca <quentin.gliosca@gmail.com>
2020-06-20 09:20:07 +0200

    Implement LCP ContentProtection and ContentProtectionService

---
+++ readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/LCPContentProtection.kt
@@ -0,0 +1,81 @@
+/*
+ * Module: r2-lcp-kotlin
+ * Developers: Quentin Gliosca
+ *
+ * Copyright (c) 2020. Readium Foundation. All rights reserved.
+ * Use of this source code is governed by a BSD-style license which is detailed in the
+ * LICENSE file present in the project repository where this source code is maintained.
+ */
+
+package org.readium.r2.lcp
+
+import org.readium.r2.shared.fetcher.Fetcher
+import org.readium.r2.shared.fetcher.TransformingFetcher
+import org.readium.r2.shared.format.Format
+import org.readium.r2.shared.publication.ContentProtection
+import org.readium.r2.shared.publication.OnAskCredentialsCallback
+import org.readium.r2.shared.publication.Publication
+import org.readium.r2.shared.util.File
+import org.readium.r2.shared.util.Try
+import java.lang.IllegalStateException
+
+class LCPContentProtection(
+    private val lcpService: LCPService,
+    private val lcpAuthenticating: LCPAuthenticating
+) : ContentProtection {
+
+    override suspend fun open(
+        file: File,
+        fetcher: Fetcher,
+        askCredentials: Boolean,
+        credentials: String?,
+        sender: Any?,
+        onAskCredentials: OnAskCredentialsCallback?
+    ): Try<ContentProtection.ProtectedFile, Publication.OpeningError>? {
+
+        val isProtectedWithLcp = when (file.format()) {
+            Format.EPUB -> fetcher.get("/META-INF/license.lcpl").length().isSuccess
+            else -> fetcher.get("/license.lcpl").length().isSuccess
+        }
+
+        if (!isProtectedWithLcp)
+            return null
+
+        return try {
+            val license = lcpService
+                .retrieveLicense(file,  lcpAuthenticating.takeIf { askCredentials })
+                .getOrThrow()
+            val protectedFile = ContentProtection.ProtectedFile(
+                file,
+                TransformingFetcher(fetcher, LCPDecryptor(license)::transform),
+                LCPContentProtectionService.createFactory(license)
+            )
+            Try.success(protectedFile)
+
+        } catch (e: LCPError) {
+            Try.failure(e.toOpeningError())
+        }
+    }
+}
+
+private fun LCPError.toOpeningError() = when (this) {
+    is LCPError.licenseIsBusy,
+    is LCPError.crlFetching,
+    is LCPError.network,
+    is LCPError.runtime,
+    is LCPError.unknown ->
+        Publication.OpeningError.Unavailable(this)
+    is LCPError.licenseIntegrity,
+    is LCPError.licenseStatus,
+    is LCPError.licenseContainer,
+    is LCPError.licenseInteractionNotAvailable,
+    is LCPError.licenseProfileNotSupported,
+    is LCPError.licenseRenew,
+    is LCPError.parsing ->
+        Publication.OpeningError.Forbidden(this)
+    else -> // this should never happen
+        if (BuildConfig.DEBUG)
+            throw IllegalStateException(this)
+        else
+            Publication.OpeningError.Unavailable(this)
+}
