a8e760a20aed5f5f7c6e52e795f3d08d9911aedb
Mickaël Menu <mickael.menu@gmail.com>
2020-08-11 09:51:55 +0200

    Require a base URL for the PDF fragment

--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pdf/R2PdfActivity.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pdf/R2PdfActivity.kt
@@ -10,6 +10,8 @@
 package org.readium.r2.navigator.pdf
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.readium.r2.navigator.NavigatorFragmentFactory
@@ -17,6 +19,7 @@ import org.readium.r2.navigator.R
 import org.readium.r2.shared.FragmentNavigator
 import org.readium.r2.shared.PdfSupport
 import org.readium.r2.shared.extensions.getPublication
+import org.readium.r2.shared.extensions.putPublication
 import org.readium.r2.shared.publication.Publication
 
 @PdfSupport
@@ -26,8 +29,10 @@ class R2PdfActivity : AppCompatActivity() {
 
     @OptIn(FragmentNavigator::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val baseUrl = intent.getStringExtra(EXTRA_BASE_URL)
+            ?: throw IllegalArgumentException("baseUrl is required")
         publication = intent.getPublication(this)
-        supportFragmentManager.fragmentFactory = NavigatorFragmentFactory(publication)
+        supportFragmentManager.fragmentFactory = NavigatorFragmentFactory(publication, baseUrl = baseUrl)
 
         super.onCreate(savedInstanceState)
 
@@ -37,6 +42,16 @@ class R2PdfActivity : AppCompatActivity() {
     override fun finish() {
         setResult(Activity.RESULT_OK, intent)
         super.finish()
+    }
+
+    companion object {
+        private const val EXTRA_BASE_URL = "baseUrl"
+
+        fun createIntent(context: Context, publication: Publication, baseUrl: String): Intent = Intent(context, R2PdfActivity::class.java).apply {
+            putPublication(publication)
+            putExtra(EXTRA_BASE_URL, baseUrl)
+        }
+
     }
 
 }
