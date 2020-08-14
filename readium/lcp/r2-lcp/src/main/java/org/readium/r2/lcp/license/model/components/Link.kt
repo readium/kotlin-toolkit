5e02f40b499ab2ab239126a03e54d9fe61a3bd0f
Mickaël Menu <mickael.menu@gmail.com>
2020-08-14 11:18:16 +0200

    Use URITemplate in Link.url()

--- readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/license/model/components/Link.kt
+++ readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/license/model/components/Link.kt
@@ -14,6 +14,7 @@ import org.json.JSONArray
 import org.json.JSONObject
 import org.readium.r2.lcp.LcpException
 import org.readium.r2.lcp.service.URLParameters
+import org.readium.r2.shared.util.URITemplate
 import java.net.URL
 
 data class Link(val json: JSONObject) {
@@ -54,26 +55,13 @@ data class Link(val json: JSONObject) {
 
     }
 
-
-//TODO: needs some more work here
     fun url(parameters:  URLParameters) : URL? {
         if (!templated) {
             return URL(href)
         }
-//        val registerUrl = URL(url.toString().replace("{?id,name}", ""))
-//        val renewUrl = URL(url.toString().replace("{?end,id,name}", ""))
 
-        var sanitized =  URL(href.replace("{?id,name}", ""))
-        sanitized =  URL(sanitized.toString().replace("{?end,id,name}", ""))
-        return sanitized
-
-
-//    val urlString = href.replacingOccurrences(of = "\\{\\?.+?\\}", with = "", options = listOf<.regularExpression>)
-//        var urlBuilder = URLComponents(string = urlString) ?: return null
-//        urlBuilder.queryItems = parameters.map { param  ->
-//            URLQueryItem(name = param.key, value = param.value.description)
-//        }
-//        return urlBuilder.url
+        val expandedHref = URITemplate(href).expand(parameters.mapValues { it.value ?: "" })
+        return URL(expandedHref)
     }
 
     val url: URL?
