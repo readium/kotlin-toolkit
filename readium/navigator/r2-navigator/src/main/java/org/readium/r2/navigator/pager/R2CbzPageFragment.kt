1f43cd8c4cfa3649c31e4360ef3b7003dcb9104d
Mickaël Menu <mickael.menu@gmail.com>
2020-11-24 12:25:09 +0100

    Add support for display cutouts in the image/CBZ navigator

--- readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2CbzPageFragment.kt
+++ readium/navigator/r2-navigator/src/main/java/org/readium/r2/navigator/pager/R2CbzPageFragment.kt
@@ -14,11 +14,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.ImageView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.readium.r2.navigator.R
+import org.readium.r2.shared.SCROLL_REF
 import org.readium.r2.shared.publication.Link
 import org.readium.r2.shared.publication.Publication
 import kotlin.coroutines.CoroutineContext
@@ -31,12 +38,18 @@ class R2CbzPageFragment(private val publication: Publication)
         get() = Dispatchers.Main
 
     private val link: Link
-        get() = requireArguments().getParcelable<Link>("link")!!
+        get() = requireArguments().getParcelable("link")!!
+
+    private var windowInsets: WindowInsetsCompat = WindowInsetsCompat.CONSUMED
+    private lateinit var containerView: View
+    private lateinit var imageView: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.viewpager_fragment_cbz, container, false)
-        val imageView = view.findViewById<ImageView>(R.id.imageView)
+        containerView = inflater.inflate(R.layout.viewpager_fragment_cbz, container, false)
+        imageView = containerView.findViewById(R.id.imageView)
+
+        setupPadding()
 
        launch {
            publication.get(link)
@@ -46,7 +59,41 @@ class R2CbzPageFragment(private val publication: Publication)
                ?.let { imageView.setImageBitmap(it) }
        }
 
-       return view
+       return containerView
+    }
+
+    private fun setupPadding() {
+        updatePadding()
+
+        // Update padding when the window insets change, for example when the navigation and status
+        // bars are toggled.
+        ViewCompat.setOnApplyWindowInsetsListener(containerView) { _, insets ->
+            windowInsets = insets
+            updatePadding()
+            insets
+        }
+    }
+
+    private fun updatePadding() {
+        val activity = activity ?: return
+        if (!viewLifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.CREATED)) {
+            return
+        }
+
+        var top = 0
+        var bottom = 0
+
+        // Add additional padding to take into account the display cutout, if needed.
+        if (
+            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P &&
+            activity.window.attributes.layoutInDisplayCutoutMode != WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+        ) {
+            val displayCutoutInsets = windowInsets.getInsets(WindowInsetsCompat.Type.displayCutout())
+            top += displayCutoutInsets.top
+            bottom += displayCutoutInsets.bottom
+        }
+
+        imageView.setPadding(0, top, 0, bottom)
     }
 
 }
