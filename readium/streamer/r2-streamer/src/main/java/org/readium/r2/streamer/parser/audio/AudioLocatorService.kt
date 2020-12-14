demarque/r2-streamer-kotlin

Add the AudioLocatorService

r2-streamer/src/main/java/org/readium/r2/streamer/parser/audio/AudioLocatorService.kt
@@ -1,0 +1,98 @@
+/*
+ * Copyright 2020 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+
+package org.readium.r2.streamer.parser.audio
+
+import org.readium.r2.shared.format.MediaType
+import org.readium.r2.shared.publication.Link
+import org.readium.r2.shared.publication.Locator
+import org.readium.r2.shared.publication.Publication
+import org.readium.r2.shared.publication.firstWithHref
+import org.readium.r2.shared.publication.services.LocatorService
+import org.readium.r2.shared.publication.services.PerResourcePositionsService
+
+/** Locator service for audio publications. */
+class AudioLocatorService(private val readingOrder: List<Link>) : LocatorService {
+
+    /** Duration per reading order index. */
+    private val durations: List<Double> =
+        readingOrder.map { it.duration ?: 0.0 }
+
+    /** Total duration of the publication. */
+    private val totalDuration: Double? =
+        durations.sum().takeIf { it > 0 }
+
+    override fun locate(locator: Locator): Locator? {
+        if (readingOrder.firstWithHref(locator.href) != null) {
+            return locator
+        }
+
+        val target = locator.locations.totalProgression?.let { locate(it) }
+        if (target != null) {
+            return target.copy(
+                title = locator.title,
+                text = locator.text
+            )
+        }
+
+        return null
+    }
+
+    override fun locate(progression: Double): Locator? {
+        totalDuration ?: return null
+        val positionInPublication = progression * totalDuration
+
+        val (link, resourcePosition) = readingOrderItemAtPosition(positionInPublication)
+            ?: return null
+
+        val positionInResource = positionInPublication - resourcePosition
+
+        return Locator(
+            href = link.href,
+            type = link.type ?: MediaType.BINARY.toString(),
+            locations = Locator.Locations(
+                fragments = listOf("t=${positionInResource.toInt()}"),
+                progression = link.duration?.let { duration ->
+                    if (duration == 0.0) 0.0
+                    else positionInResource / duration
+                },
+                totalProgression = progression
+            )
+        )
+    }
+
+    /**
+     * Finds the reading order item containing the time [position] (in seconds), as well as its
+     * start time.
+     */
+    private fun readingOrderItemAtPosition(position: Double): Pair<Link, Double>? {
+        var current: Double = 0.0
+        for ((i, duration) in durations.withIndex()) {
+            val link = readingOrder[i]
+            val itemDuration = link.duration ?: 0.0
+            if (position >= current && position < (current + itemDuration)) {
+                return Pair(link, current)
+            }
+
+            current += itemDuration
+        }
+
+        if (position == totalDuration) {
+            return readingOrder.lastOrNull()?.let { Pair(it, current - (it.duration ?: 0.0)) }
+        }
+
+        return null
+    }
+
+    companion object {
+
+        fun createFactory(): (Publication.Service.Context) -> AudioLocatorService = {
+            AudioLocatorService(readingOrder = it.manifest.readingOrder)
+        }
+
+    }
+
+}

r2-streamer/src/main/java/org/readium/r2/streamer/parser/readium/ReadiumWebPubParser.kt
@@ -20,6 +20,8 @@ import org.readium.r2.shared.format.MediaType
 import org.readium.r2.shared.publication.Manifest
 import org.readium.r2.shared.publication.Publication
 import org.readium.r2.shared.publication.services.PerResourcePositionsService
+import org.readium.r2.shared.publication.services.locatorServiceFactory
+import org.readium.r2.shared.publication.services.positionsServiceFactory
 import org.readium.r2.shared.util.File
 
 import org.readium.r2.shared.util.logging.WarningLogger
@@ -30,6 +32,7 @@ import org.readium.r2.streamer.container.PublicationContainer
 import org.readium.r2.streamer.extensions.fromFile
 import org.readium.r2.streamer.fetcher.LcpDecryptor
 import org.readium.r2.streamer.parser.PubBox
+import org.readium.r2.streamer.parser.audio.AudioLocatorService
 import org.readium.r2.streamer.toPublicationType
 import java.io.FileNotFoundException
 
@@ -44,12 +47,13 @@ class ReadiumWebPubParser(private val openPdf: OpenPdfDocument? = null) : Public
         fetcher: Fetcher,
         warnings: WarningLogger?
     ): Publication.Builder? {
+        val format = file.format() ?: return null
 
-        if (file.format()?.mediaType?.isReadiumWebPubProfile != true)
+        if (!format.mediaType.isReadiumWebPubProfile)
             return null
 
         val manifest =
-            if (file.format()?.mediaType?.isRwpm == true) {
+            if (format.mediaType.isRwpm) {
                 val manifestLink = fetcher.links().firstOrNull()
                     ?: error("Empty fetcher.")
                 val manifestJson = fetcher.get(manifestLink).use {
@@ -71,20 +75,20 @@ class ReadiumWebPubParser(private val openPdf: OpenPdfDocument? = null) : Public
         // Checks the requirements from the LCPDF specification.
         // https://readium.org/lcp-specs/notes/lcp-for-pdf.html
         val readingOrder = manifest.readingOrder
-        if (file.format() == Format.LCP_PROTECTED_PDF && (readingOrder.isEmpty() || !readingOrder.all { it.mediaType?.matches(MediaType.PDF) == true })) {
+        if (format == Format.LCP_PROTECTED_PDF && (readingOrder.isEmpty() || !readingOrder.all { it.mediaType?.matches(MediaType.PDF) == true })) {
             throw Exception("Invalid LCP Protected PDF.")
         }
 
-        val positionsService = when(file.format()) {
-            Format.LCP_PROTECTED_PDF ->
-                openPdf?.let { LcpdfPositionsService.create(it) }
-            Format.READIUM_AUDIOBOOK_MANIFEST, Format.READIUM_AUDIOBOOK, Format.LCP_PROTECTED_AUDIOBOOK ->
-                PerResourcePositionsService.createFactory(fallbackMediaType = "audio/*")
-            Format.DIVINA_MANIFEST, Format.DIVINA ->
-                PerResourcePositionsService.createFactory("image/*")
-            else -> null
+        val servicesBuilder = Publication.ServicesBuilder().apply {
+            when (format) {
+                Format.LCP_PROTECTED_PDF ->
+                    positionsServiceFactory = openPdf?.let { LcpdfPositionsService.create(it) }
+                Format.DIVINA_MANIFEST, Format.DIVINA ->
+                    positionsServiceFactory = PerResourcePositionsService.createFactory("image/*")
+                Format.READIUM_AUDIOBOOK, Format.READIUM_AUDIOBOOK_MANIFEST, Format.LCP_PROTECTED_AUDIOBOOK ->
+                    locatorServiceFactory = AudioLocatorService.createFactory()
+            }
         }
-        val servicesBuilder = Publication.ServicesBuilder(positions = positionsService)
 
         return Publication.Builder(manifest, fetcher, servicesBuilder)
     }

readium/kotlin-toolkit

readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/audio/AudioLocatorService.kt
@@ -1,0 +1,97 @@
+/*
+ * Copyright 2020 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+
+package org.readium.r2.streamer.parser.audio
+
+import org.readium.r2.shared.publication.Link
+import org.readium.r2.shared.publication.Locator
+import org.readium.r2.shared.publication.Publication
+import org.readium.r2.shared.publication.firstWithHref
+import org.readium.r2.shared.publication.services.LocatorService
+import org.readium.r2.shared.util.mediatype.MediaType
+
+/** Locator service for audio publications. */
+class AudioLocatorService(private val readingOrder: List<Link>) : LocatorService {
+
+    /** Duration per reading order index. */
+    private val durations: List<Double> =
+        readingOrder.map { it.duration ?: 0.0 }
+
+    /** Total duration of the publication. */
+    private val totalDuration: Double? =
+        durations.sum().takeIf { it > 0 }
+
+    override fun locate(locator: Locator): Locator? {
+        if (readingOrder.firstWithHref(locator.href) != null) {
+            return locator
+        }
+
+        val target = locator.locations.totalProgression?.let { locate(it) }
+        if (target != null) {
+            return target.copy(
+                title = locator.title,
+                text = locator.text
+            )
+        }
+
+        return null
+    }
+
+    override fun locate(progression: Double): Locator? {
+        totalDuration ?: return null
+        val positionInPublication = progression * totalDuration
+
+        val (link, resourcePosition) = readingOrderItemAtPosition(positionInPublication)
+            ?: return null
+
+        val positionInResource = positionInPublication - resourcePosition
+
+        return Locator(
+            href = link.href,
+            type = link.type ?: MediaType.BINARY.toString(),
+            locations = Locator.Locations(
+                fragments = listOf("t=${positionInResource.toInt()}"),
+                progression = link.duration?.let { duration ->
+                    if (duration == 0.0) 0.0
+                    else positionInResource / duration
+                },
+                totalProgression = progression
+            )
+        )
+    }
+
+    /**
+     * Finds the reading order item containing the time [position] (in seconds), as well as its
+     * start time.
+     */
+    private fun readingOrderItemAtPosition(position: Double): Pair<Link, Double>? {
+        var current: Double = 0.0
+        for ((i, duration) in durations.withIndex()) {
+            val link = readingOrder[i]
+            val itemDuration = link.duration ?: 0.0
+            if (position >= current && position < (current + itemDuration)) {
+                return Pair(link, current)
+            }
+
+            current += itemDuration
+        }
+
+        if (position == totalDuration) {
+            return readingOrder.lastOrNull()?.let { Pair(it, current - (it.duration ?: 0.0)) }
+        }
+
+        return null
+    }
+
+    companion object {
+
+        fun createFactory(): (Publication.Service.Context) -> AudioLocatorService = {
+            AudioLocatorService(readingOrder = it.manifest.readingOrder)
+        }
+
+    }
+
+}

readium/streamer/r2-streamer/src/main/java/org/readium/r2/streamer/parser/readium/ReadiumWebPubParser.kt
@@ -20,6 +20,8 @@ import org.readium.r2.shared.util.mediatype.MediaType
 import org.readium.r2.shared.publication.Manifest
 import org.readium.r2.shared.publication.Publication
 import org.readium.r2.shared.publication.services.PerResourcePositionsService
+import org.readium.r2.shared.publication.services.locatorServiceFactory
+import org.readium.r2.shared.publication.services.positionsServiceFactory
 import org.readium.r2.shared.util.File
 
 import org.readium.r2.shared.util.logging.WarningLogger
@@ -31,6 +33,7 @@ import org.readium.r2.streamer.container.PublicationContainer
 import org.readium.r2.streamer.extensions.fromFile
 import org.readium.r2.streamer.fetcher.LcpDecryptor
 import org.readium.r2.streamer.parser.PubBox
+import org.readium.r2.streamer.parser.audio.AudioLocatorService
 import org.readium.r2.streamer.toPublicationType
 import java.io.FileNotFoundException
 
@@ -78,14 +81,24 @@ class ReadiumWebPubParser(private val pdfFactory: PdfDocumentFactory? = null) : 
             throw Exception("Invalid LCP Protected PDF.")
         }
 
-        val positionsService = when(file.mediaType()) {
-            MediaType.LCP_PROTECTED_PDF ->
-                pdfFactory?.let { LcpdfPositionsService.create(it) }
-            MediaType.DIVINA_MANIFEST, MediaType.DIVINA ->
-                PerResourcePositionsService.createFactory("image/*")
+        val locatorService = when (file.mediaType()) {
+            MediaType.READIUM_AUDIOBOOK, MediaType.READIUM_AUDIOBOOK_MANIFEST, MediaType.LCP_PROTECTED_AUDIOBOOK ->
+                AudioLocatorService.createFactory()
             else -> null
         }
-        val servicesBuilder = Publication.ServicesBuilder(positions = positionsService)
+
+        val servicesBuilder = Publication.ServicesBuilder().apply {
+            when (file.mediaType()) {
+                MediaType.LCP_PROTECTED_PDF ->
+                    positionsServiceFactory = pdfFactory?.let { LcpdfPositionsService.create(it) }
+
+                MediaType.DIVINA_MANIFEST, MediaType.DIVINA ->
+                    positionsServiceFactory = PerResourcePositionsService.createFactory("image/*")
+
+                MediaType.READIUM_AUDIOBOOK, MediaType.READIUM_AUDIOBOOK_MANIFEST, MediaType.LCP_PROTECTED_AUDIOBOOK ->
+                    locatorServiceFactory = AudioLocatorService.createFactory()
+            }
+        }
 
         return Publication.Builder(manifest, fetcher, servicesBuilder)
     }