dd5c1f6615f6173932bd22d62434648a548d10ed
qnga <32197639+qnga@users.noreply.github.com>
2021-03-11 11:41:43 +0100

    Cache FullLcpResource (#107)

--- readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/LcpDecryptor.kt
+++ readium/lcp/r2-lcp/src/main/java/org/readium/r2/lcp/LcpDecryptor.kt
@@ -54,7 +54,7 @@ internal class LcpDecryptor(val license: LcpLicense?) {
     private class FullLcpResource(
         resource: Resource,
         private val license: LcpLicense
-    ) : TransformingResource(resource) {
+    ) : TransformingResource(resource, cacheBytes = true) {
 
         override suspend fun transform(data: ResourceTry<ByteArray>): ResourceTry<ByteArray> =
             license.decryptFully(data, resource.link().isDeflated)
