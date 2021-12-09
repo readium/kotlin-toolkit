af501525a36f3b00cecdac4faa43d1a05bbf1c86
Mickaël Menu <mickael.menu@gmail.com>
2021-12-09 18:10:44 +0100

    Extract the PDF parser

---
+++ readium/streamer/pdf/src/main/java/org/readium/r2/streamer/parser/pdf/PdfiumDocument.kt
@@ -0,0 +1,11 @@
+/*
+ * Module: r2-shared-kotlin
+ * Developers: Mickaël Menu
+ *
+ * Copyright (c) 2020. Readium Foundation. All rights reserved.
+ * Use of this source code is governed by a BSD-style license which is detailed in the
+ * LICENSE file present in the project repository where this source code is maintained.
+ */
+
+package org.readium.r2.streamer.parser.pdf
+
