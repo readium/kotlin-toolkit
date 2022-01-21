baf4646e3830c3916bbf9870998e6d681549dd1d
Mickaël Menu <mickael.menu@gmail.com>
2022-01-21 12:00:26 +0100

    Revert MediaMetadata.METADATA_KEY_TITLE

--- readium/navigator-media2/src/main/java/org/readium/navigator/media2/DefaultMetadataFactory.kt
+++ readium/navigator-media2/src/main/java/org/readium/navigator/media2/DefaultMetadataFactory.kt
@@ -52,7 +52,7 @@ internal class DefaultMetadataFactory(private val publication: Publication): Med
         val link = publication.readingOrder[index]
         builder.putLong(MediaMetadata.METADATA_KEY_TRACK_NUMBER, index.toLong())
         builder.putString(MediaMetadata.METADATA_KEY_MEDIA_URI, link.href)
-        builder.putString(MediaMetadata.METADATA_KEY_TITLE, link.title ?: publication.metadata.title)
+        builder.putString(MediaMetadata.METADATA_KEY_TITLE, link.title)
         builder.putString(MediaMetadata.METADATA_KEY_DISPLAY_SUBTITLE, publication.metadata.title)
         builder.putString(MediaMetadata.METADATA_KEY_ALBUM, publication.metadata.title)
         builder.putLong(MediaMetadata.METADATA_KEY_DURATION, (link.duration?.toLong() ?: 0) * 1000)
