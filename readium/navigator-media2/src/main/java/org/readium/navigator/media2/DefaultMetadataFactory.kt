c571ef90cb6c2fce55295fe4b7da0d576728cc18
Mickaël Menu <mickael.menu@gmail.com>
2022-01-21 11:25:08 +0100

    Adjust metadata

--- readium/navigator-media2/src/main/java/org/readium/navigator/media2/DefaultMetadataFactory.kt
+++ readium/navigator-media2/src/main/java/org/readium/navigator/media2/DefaultMetadataFactory.kt
@@ -27,11 +27,13 @@ internal class DefaultMetadataFactory(private val publication: Publication): Med
         val builder = MediaMetadata.Builder()
             .putString(MediaMetadata.METADATA_KEY_TITLE, publication.metadata.title)
             .putString(MediaMetadata.METADATA_KEY_ALBUM, publication.metadata.title)
-            .putString(MediaMetadata.METADATA_KEY_DISPLAY_SUBTITLE, publication.metadata.title)
             .putLong(MediaMetadata.METADATA_KEY_NUM_TRACKS, publication.readingOrder.size.toLong())
 
         authors
-            ?.let { builder.putString(MediaMetadata.METADATA_KEY_AUTHOR,  it) }
+            ?.let {
+                builder.putString(MediaMetadata.METADATA_KEY_AUTHOR,  it)
+                builder.putString(MediaMetadata.METADATA_KEY_ARTIST,  it)
+            }
 
         publication.metadata.duration
             ?.let { builder.putLong(MediaMetadata.METADATA_KEY_DURATION, it.toLong() * 1000) }
@@ -43,16 +45,22 @@ internal class DefaultMetadataFactory(private val publication: Publication): Med
     }
 
     override suspend fun resourceMetadata(index: Int): MediaMetadata {
+        // See the implementation for how each metadata is used in media2:
+        // https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:media2/media2-session/src/main/java/androidx/media2/session/MediaNotificationHandler.java;l=175?q=MediaNotificationHandler
+
         val builder = MediaMetadata.Builder()
         val link = publication.readingOrder[index]
         builder.putLong(MediaMetadata.METADATA_KEY_TRACK_NUMBER, index.toLong())
         builder.putString(MediaMetadata.METADATA_KEY_MEDIA_URI, link.href)
-        builder.putString(MediaMetadata.METADATA_KEY_TITLE, link.title)
-        builder.putString(MediaMetadata.METADATA_KEY_ALBUM, publication.metadata.title)
+        builder.putString(MediaMetadata.METADATA_KEY_TITLE, link.title ?: publication.metadata.title)
         builder.putString(MediaMetadata.METADATA_KEY_DISPLAY_SUBTITLE, publication.metadata.title)
+        builder.putString(MediaMetadata.METADATA_KEY_ALBUM, publication.metadata.title)
         builder.putLong(MediaMetadata.METADATA_KEY_DURATION, (link.duration?.toLong() ?: 0) * 1000)
 
-        authors?.let { builder.putString(MediaMetadata.METADATA_KEY_AUTHOR, it) }
+        authors?.let {
+            builder.putString(MediaMetadata.METADATA_KEY_ARTIST, it)
+            builder.putString(MediaMetadata.METADATA_KEY_AUTHOR, it)
+        }
         cover.await()?.let { builder.putBitmap(MediaMetadata.METADATA_KEY_ART, it)}
         return builder.build()
     }
