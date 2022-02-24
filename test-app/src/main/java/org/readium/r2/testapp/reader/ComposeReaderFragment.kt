cbb699a51d3ef287dfc687dc6fe7555959dea4ea
Quentin Gliosca <quentin.gliosca@gmail.com>
2022-02-24 15:08:30 +0100

    Cosmetic changes

--- test-app/src/main/java/org/readium/r2/testapp/reader/ComposeReaderFragment.kt
+++ test-app/src/main/java/org/readium/r2/testapp/reader/ComposeReaderFragment.kt
@@ -1,35 +1,18 @@
 package org.readium.r2.testapp.reader
 
-import android.graphics.PointF
 import android.os.Bundle
-import android.view.*
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import org.readium.r2.lcp.lcpLicense
 import org.readium.r2.navigator.Navigator
 import org.readium.r2.navigator2.view.NavigatorListener
 import org.readium.r2.navigator3.Navigator
-import org.readium.r2.navigator3.NavigatorState
-import org.readium.r2.navigator3.Overflow
-import org.readium.r2.navigator3.ReadingProgression
 import org.readium.r2.shared.publication.Locator
-import org.readium.r2.shared.publication.Publication
-import org.readium.r2.shared.publication.indexOfFirstWithHref
-import org.readium.r2.shared.publication.toLocator
-import org.readium.r2.testapp.R
 import org.readium.r2.testapp.databinding.Reader3FragmentReaderBinding
-import org.readium.r2.testapp.reader.ReaderViewModel
-import org.readium.r2.testapp.reader.VisualReaderFragment
-import org.readium.r2.testapp.utils.toggleSystemUi
 import org.readium.r2.testapp.utils.viewLifecycle
-import timber.log.Timber
-import java.net.URL
 
 class ComposeReaderFragment : VisualReaderFragment(), NavigatorListener {
 
