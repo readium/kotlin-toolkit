337a152663e2fe73bbe7155a9b34623b78770a92
Mickaël Menu <mickael.menu@gmail.com>
2022-06-02 11:17:11 +0200

    Upgrade Gradle and AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@
 import org.jetbrains.dokka.gradle.DokkaTaskPartial
 
 plugins {
-    id("com.android.application") version "7.1.3" apply false
-    id("com.android.library") version "7.1.3" apply false
+    id("com.android.application") version "7.2.1" apply false
+    id("com.android.library") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
     id("org.jetbrains.dokka") version "1.6.20" apply true
 }
