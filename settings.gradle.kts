3c6d61a208dd7e28e75049528d9a004f0a8050c7
Mickaël Menu <mickael.menu@gmail.com>
2022-06-02 16:19:08 +0200

    Revert "Move Gradle plugins versions into the root build.gradle.kts"
    This reverts commit 1ef96e0688ba741aeb383770c4a2b692996fbdab.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,16 @@ pluginManagement {
         maven(url = "https://jitpack.io")
         maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
     }
+
+    // Setting the plugin versions here doesn't work with AGP Upgrade Assistant, but we need
+    // it to integrate Readium in submodules.
+    // See https://github.com/readium/kotlin-toolkit/pull/97
+    plugins {
+        id("com.android.application") version ("7.2.1")
+        id("com.android.library") version ("7.2.1")
+        id("org.jetbrains.kotlin.android") version ("1.6.21")
+        id("org.jetbrains.dokka") version ("1.6.20")
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
