92a8caac370062ad6e8502b8fe07971515b7df36
qnga <32197639+qnga@users.noreply.github.com>
2022-09-20 14:43:21 +0200

    Add accessibility metadata (#269)

--- readium/streamer/src/main/java/org/readium/r2/streamer/parser/epub/NcxParser.kt
+++ readium/streamer/src/main/java/org/readium/r2/streamer/parser/epub/NcxParser.kt
@@ -1,10 +1,7 @@
 /*
- * Module: r2-streamer-kotlin
- * Developers: Quentin Gliosca
- *
- * Copyright (c) 2018. Readium Foundation. All rights reserved.
- * Use of this source code is governed by a BSD-style license which is detailed in the
- * LICENSE file present in the project repository where this source code is maintained.
+ * Copyright 2022 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
  */
 
 package org.readium.r2.streamer.parser.epub
--- readium/streamer/src/main/java/org/readium/r2/streamer/parser/epub/PackageDocument.kt
+++ readium/streamer/src/main/java/org/readium/r2/streamer/parser/epub/PackageDocument.kt
@@ -1,10 +1,7 @@
 /*
- * Module: r2-streamer-kotlin
- * Developers: Quentin Gliosca
- *
- * Copyright (c) 2018. Readium Foundation. All rights reserved.
- * Use of this source code is governed by a BSD-style license which is detailed in the
- * LICENSE file present in the project repository where this source code is maintained.
+ * Copyright 2022 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
  */
 
 package org.readium.r2.streamer.parser.epub
@@ -17,7 +14,7 @@ internal data class PackageDocument(
     val path: String,
     val epubVersion: Double,
     val uniqueIdentifierId: String?,
-    val metadata: EpubMetadata,
+    val metadata: List<MetadataItem>,
     val manifest: List<Item>,
     val spine: Spine
 ) {
