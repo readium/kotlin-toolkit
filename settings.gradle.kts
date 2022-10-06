0de59a43d0328f595f858c32eb478a8c43bd1e26
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2022-10-05 23:31:35 -0500

    Update Kotlin to 1.7.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ pluginManagement {
     plugins {
         id("com.android.application") version ("7.3.0")
         id("com.android.library") version ("7.3.0")
-        id("org.jetbrains.kotlin.android") version ("1.7.10")
+        id("org.jetbrains.kotlin.android") version ("1.7.20")
         id("org.jetbrains.dokka") version ("1.7.10")
         id("org.jetbrains.kotlin.plugin.serialization") version ("1.7.10")
     }
