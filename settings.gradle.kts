ca166e9d44f57ef0d463b4dde652bc2f8fc20377
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2022-10-31 21:32:27 -0500

    Update dokka to 1.7.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ pluginManagement {
         id("com.android.application") version ("7.3.1")
         id("com.android.library") version ("7.3.1")
         id("org.jetbrains.kotlin.android") version ("1.7.20")
-        id("org.jetbrains.dokka") version ("1.7.10")
+        id("org.jetbrains.dokka") version ("1.7.20")
         id("org.jetbrains.kotlin.plugin.serialization") version ("1.7.10")
     }
 }
