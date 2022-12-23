6769107f4a3ca72071be60c78e1fc06eee403dfb
Mickaël Menu <mickael.menu@gmail.com>
2022-12-23 12:40:59 +0100

    Remove dependency on JitPack

--- readium/adapters/pdfium/pdfium-navigator/build.gradle.kts
+++ readium/adapters/pdfium/pdfium-navigator/build.gradle.kts
@@ -62,8 +62,6 @@ publishing {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-
     api(project(":readium:readium-shared"))
     api(project(":readium:readium-navigator"))
     api(project(":readium:adapters:pdfium:readium-adapter-pdfium-document"))
