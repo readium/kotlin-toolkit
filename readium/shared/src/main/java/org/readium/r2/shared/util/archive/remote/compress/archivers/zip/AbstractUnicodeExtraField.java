60936d7681c7d417b1845f73e690fc598dd82f17
Quentin Gliosca <quentin.gliosca@gmail.com>
2023-04-14 11:20:49 +0200

    Update licenses

--- readium/shared/src/main/java/org/readium/r2/shared/util/archive/remote/compress/archivers/zip/AbstractUnicodeExtraField.java
+++ readium/shared/src/main/java/org/readium/r2/shared/util/archive/remote/compress/archivers/zip/AbstractUnicodeExtraField.java
@@ -1,4 +1,10 @@
 /*
+ * Copyright 2023 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+
+/*
  *  Licensed to the Apache Software Foundation (ASF) under one or more
  *  contributor license agreements.  See the NOTICE file distributed with
  *  this work for additional information regarding copyright ownership.
--- readium/streamer/src/main/java/org/readium/r2/streamer/parser/PublicationParser.kt
+++ readium/streamer/src/main/java/org/readium/r2/streamer/parser/PublicationParser.kt
@@ -18,14 +18,7 @@ interface PublicationParser {
     /**
      * Constructs a [Publication.Builder] to build a [Publication] from a publication asset.
      *
-     * @param assetName Name of the asset, e.g. a filename.
-     * @param mediaType Media type of the publication.
-     * @param fetcher Initial leaf fetcher which should be used to read the publication's resources.
-     * This can be used to:
-     * - support content protection technologies
-     * - parse exploded archives or in archiving formats unknown to the parser, e.g. RAR
-     * If the asset is not an archive, it will be reachable at the HREF /<asset.name>,
-     * e.g. with a PDF.
+     * @param asset Publication asset.
      * @param warnings Used to report non-fatal parsing warnings, such as publication authoring
      * mistakes. This is useful to warn users of potential rendering issues or help authors
      * debug their publications.
