b24160b37fa2d3de1c82e46ca9eb122cf0f9e095
Quentin Gliosca <quentin.gliosca@gmail.com>
2023-04-20 11:16:31 +0200

    Lint

--- readium/shared/src/main/java/org/readium/r2/shared/util/Deprecated.kt
+++ readium/shared/src/main/java/org/readium/r2/shared/util/Deprecated.kt
@@ -17,6 +17,7 @@ typealias File = FileAsset
  * Returns the encapsulated result of the given transform function applied to the encapsulated |Throwable] exception
  * if this instance represents failure or the original encapsulated value if it is success.
  */
+@Suppress("Unused_parameter")
 @Deprecated(message = "Use getOrElse instead.", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("getOrElse"))
 inline fun <R, S : R, F : Throwable> Try<S, F>.recover(transform: (exception: F) -> R): Try<R, Nothing> =
     TODO()
