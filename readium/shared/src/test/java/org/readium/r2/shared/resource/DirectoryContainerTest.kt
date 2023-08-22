0c2fce1f73e1326d5c2c78dd3d29f8cee2546188
Mickaël Menu <mickael.menu@gmail.com>
2023-08-22 12:50:41 +0200

    Fix tests

--- readium/shared/src/test/java/org/readium/r2/shared/resource/DirectoryContainerTest.kt
+++ readium/shared/src/test/java/org/readium/r2/shared/resource/DirectoryContainerTest.kt
@@ -132,7 +132,7 @@ class DirectoryContainerTest {
             }
 
             val entries = sut().entries()
-            assertThat(entries?.map { it.path }).containsExactly(
+            assertThat(entries?.map { it.path }).contains(
                 "/subdirectory/hello.mp3",
                 "/subdirectory/text2.txt",
                 "/text1.txt"
