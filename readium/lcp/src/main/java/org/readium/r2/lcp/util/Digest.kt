07e6531290d99181404d91996d39f20d8f050f7f
Quentin Gliosca <quentin.gliosca@gmail.com>
2024-01-11 19:13:28 +0100

    Check LCP acqusition hashes + fixes

---
+++ readium/lcp/src/main/java/org/readium/r2/lcp/util/Digest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2023 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+
+package org.readium.r2.lcp.util
+
+import java.io.File
+import java.security.MessageDigest
+import kotlin.io.encoding.ExperimentalEncodingApi
+import org.readium.r2.shared.extensions.tryOrNull
+
+/**
+ * Returns the SHA-256 sum of a file content encoded in base64 or null if computation failed.
+ */
+@OptIn(ExperimentalEncodingApi::class)
+internal fun File.sha256(): ByteArray? =
+    tryOrNull<ByteArray> {
+        val md = MessageDigest.getInstance("SHA-256")
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        inputStream().use {
+            var bytes = it.read(buffer)
+            while (bytes >= 0) {
+                md.update(buffer, 0, bytes)
+                bytes = it.read(buffer)
+            }
+        }
+        return md.digest()
+    }
