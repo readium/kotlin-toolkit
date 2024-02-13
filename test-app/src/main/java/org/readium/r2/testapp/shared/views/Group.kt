c65e9c38fb93e134edcdbec23059840db423342b
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2024-02-12 22:09:23 -0600

    Create and user new EmphasisProvider

--- test-app/src/main/java/org/readium/r2/testapp/shared/views/Group.kt
+++ test-app/src/main/java/org/readium/r2/testapp/shared/views/Group.kt
@@ -6,36 +6,21 @@
 
 package org.readium.r2.testapp.shared.views
 
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import org.readium.r2.testapp.utils.compose.Emphasis
+import org.readium.r2.testapp.utils.compose.EmphasisProvider
+import org.readium.r2.testapp.utils.compose.LocalContentEmphasis
 
 /**
  * Sets the emphasis (alpha) of a group of [Composable] views.
  */
 @Composable
 fun Group(lowEmphasis: Boolean = false, enabled: Boolean = true, content: @Composable () -> Unit) {
-    val contentColor = when {
-        !enabled -> 0.38f
-        else -> 1.0f
+    val emphasis = when {
+        !enabled -> Emphasis.Disabled
+        else -> Emphasis.Medium
     }
-    val fontWeight = when {
-        !lowEmphasis -> FontWeight.Normal
-        else -> FontWeight.Bold
+    EmphasisProvider(LocalContentEmphasis provides emphasis) {
+        content()
     }
-
-    CompositionLocalProvider(
-        value = LocalContentColor provides MaterialTheme.colorScheme.onSurface.copy(
-            alpha = contentColor
-        ),
-        content = {
-            ProvideTextStyle(value = TextStyle(fontWeight = fontWeight)) {
-                content()
-            }
-        }
-    )
 }
