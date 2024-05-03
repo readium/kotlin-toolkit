8fb7ccbe285c94cdb90b4f9a8e0aa90dc03b0f5d
Mickaël Menu <mickael.menu@gmail.com>
2024-05-03 16:51:40 +0200

    Change LCP device name strategy (#508)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,10 @@ All notable changes to this project will be documented in this file. Take a look
 
 * All the APIs using or returning a `Date` objects are now using a custom `Instant` type. 
 
+#### LCP
+
+* [#493](https://github.com/readium/kotlin-toolkit/discussions/493) The LCP module does not require the Bluetooth permissions anymore to derive the device name.
+
 ### Fixed
 
 #### Navigator
--- readium/lcp/src/main/java/org/readium/r2/lcp/LcpService.kt
+++ readium/lcp/src/main/java/org/readium/r2/lcp/LcpService.kt
@@ -166,10 +166,15 @@ public interface LcpService {
 
         /**
          * LCP service factory.
+         *
+         * @param deviceName Device name used when registering a license to an LSD server.
+         * If not provided, the device name will be generated from the device's manufacturer and
+         * model.
          */
         public operator fun invoke(
             context: Context,
-            assetRetriever: AssetRetriever
+            assetRetriever: AssetRetriever,
+            deviceName: String? = null
         ): LcpService? {
             if (!LcpClient.isAvailable()) {
                 return null
@@ -181,6 +186,7 @@ public interface LcpService {
             val licenseRepository = LicensesRepository(db)
             val network = NetworkService()
             val device = DeviceService(
+                deviceName = deviceName,
                 repository = deviceRepository,
                 network = network,
                 context = context
