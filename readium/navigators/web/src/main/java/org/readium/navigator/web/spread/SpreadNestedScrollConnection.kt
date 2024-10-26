f71e2d3ce21a012294017de3723d77e7d346d772
Quentin Gliosca <quentin.gliosca@gmail.com>
2024-10-26 14:41:22 +0200

    Improve scroll algorithm to prevent users from being locked on one page.

--- readium/navigators/web/src/main/java/org/readium/navigator/web/spread/SpreadNestedScrollConnection.kt
+++ readium/navigators/web/src/main/java/org/readium/navigator/web/spread/SpreadNestedScrollConnection.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.unit.Velocity
 import kotlin.math.abs
+import org.readium.navigator.web.webview.RelaxedWebView
 import org.readium.navigator.web.webview.WebViewScrollable2DState
 
 internal class SpreadNestedScrollConnection(
@@ -14,9 +15,14 @@ internal class SpreadNestedScrollConnection(
     override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
         val webViewNow = webviewState.webView ?: return Offset.Zero
 
+        // For some reason, scrollX can vary by 1 or 2 pixels without any call to scrollTo.
         val webViewCannotScrollHorizontally =
-            (webViewNow.scrollX < 1 && available.x > 0) ||
-                ((webViewNow.maxScrollX - webViewNow.scrollX) < 1 && available.x < 0)
+            (webViewNow.scrollX < 3 && available.x > 0) ||
+                ((webViewNow.maxScrollX - webViewNow.scrollX) < 3 && available.x < 0)
+
+        if (webViewCannotScrollHorizontally) {
+            snapWebview(webViewNow)
+        }
 
         val isGestureHorizontal =
             (abs(available.y) / abs(available.x)) < 0.58 // tan(Pi/6)
@@ -32,13 +38,15 @@ internal class SpreadNestedScrollConnection(
 
     override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
         val webViewNow = webviewState.webView ?: return Velocity.Zero
-
-        if ((webViewNow.maxScrollX - webViewNow.scrollX) < 15) {
-            webViewNow.scrollTo(webViewNow.maxScrollX, webViewNow.scrollY)
-        } else if (webViewNow.scrollX in (0 until 15)) {
-            webViewNow.scrollTo(0, webViewNow.scrollY)
-        }
-
+        snapWebview(webViewNow)
         return Velocity.Zero
+    }
+
+    private fun snapWebview(webview: RelaxedWebView) {
+        if ((webview.maxScrollX - webview.scrollX) < 15) {
+            webview.scrollTo(webview.maxScrollX, webview.scrollY)
+        } else if (webview.scrollX in (0 until 15)) {
+            webview.scrollTo(0, webview.scrollY)
+        }
     }
 }
