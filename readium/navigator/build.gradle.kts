c69eb8d3d8cbb79c4feeb4ec1c3a780a7822b788
Franklin De Asis <franklin.deasis@alc-tech.com.ph>
2024-12-02 11:55:55 +0800

    BOOCO-B-727 add s3 publish code in build.gradle.kts

--- readium/navigator/build.gradle.kts
+++ readium/navigator/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.net.URI
+import java.util.Properties
+
 /*
  * Copyright 2018 Readium Foundation. All rights reserved.
  * Use of this source code is governed by the BSD-style license
@@ -53,4 +56,35 @@ dependencies {
     testImplementation(libs.kotlin.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.robolectric)
+}
+
+fun Project.publishing(configure: PublishingExtension.() -> Unit) =
+    extensions.configure<PublishingExtension>(configure)
+
+publishing {
+    publications {
+        withType<MavenPublication>().all {
+            groupId = "org.readium.r2"
+            artifactId = "${project.name}-android"
+            version = System.getenv("VERSION") ?: "123.456.780"
+        }
+    }
+
+    repositories {
+        maven {
+            val type = System.getenv("VERSION_TYPE") ?: "snapshots"
+            url = URI("s3://booco-android-readium/maven/$type")
+            val prop by lazy {
+                File("shared.properties")
+                    .takeIf { it.exists() }
+                    ?.let { Properties().apply { load(it.inputStream()) } }
+            }
+            credentials(AwsCredentials::class) {
+                accessKey = "BOOCO_AWS_ACCESS_KEY_ID"
+                    .let { prop?.getProperty(it) ?: System.getenv(it) }
+                secretKey = "BOOCO_AWS_SECRET_ACCESS_KEY"
+                    .let { prop?.getProperty(it) ?: System.getenv(it) }
+            }
+        }
+    }
 }
