d973ec8f9d86db339c68dc2819e44624f7dd7219
Mickaël Menu <mickael.menu@gmail.com>
2025-05-20 12:12:18 +0200

    Refactoring

--- readium/navigator/src/main/java/org/readium/r2/navigator/epub/EpubNavigatorFragment.kt
+++ readium/navigator/src/main/java/org/readium/r2/navigator/epub/EpubNavigatorFragment.kt
@@ -285,8 +285,6 @@ public class EpubNavigatorFragment internal constructor(
 
     private var state: State = State.Initializing
 
-    private var pageChangeListener: ViewPager.OnPageChangeListener? = null
-
     // Configurable
 
     override val settings: StateFlow<EpubSettings> get() = viewModel.settings
@@ -414,38 +412,6 @@ public class EpubNavigatorFragment internal constructor(
         resourcePager = binding.resourcePager
         resetResourcePager()
 
-        pageChangeListener = object : ViewPager.SimpleOnPageChangeListener() {
-            override fun onPageSelected(position: Int) {
-//                if (viewModel.layout == EpubLayout.REFLOWABLE) {
-//                    resourcePager.disableTouchEvents = true
-//                }
-                currentReflowablePageFragment?.webView?.let { webView ->
-                    if (viewModel.isScrollEnabled.value) {
-                        if (currentPagerPosition < position) {
-                            // handle swipe LEFT
-                            webView.scrollToStart()
-                        } else if (currentPagerPosition > position) {
-                            // handle swipe RIGHT
-                            webView.scrollToEnd()
-                        }
-                    } else {
-                        if (currentPagerPosition < position) {
-                            // handle swipe LEFT
-                            webView.setCurrentItem(0, false)
-                        } else if (currentPagerPosition > position) {
-                            // handle swipe RIGHT
-                            webView.setCurrentItem(webView.numPages - 1, false)
-                        }
-                    }
-                }
-                currentPagerPosition = position // Update current position
-
-                notifyCurrentLocation()
-            }
-        }
-
-        pageChangeListener?.let { resourcePager.addOnPageChangeListener(it) }
-
         // Fixed layout publications cannot intercept JS events yet.
         if (publication.metadata.presentation.layout == EpubLayout.FIXED) {
             view = KeyInterceptorView(view, inputListener)
@@ -471,10 +437,38 @@ public class EpubNavigatorFragment internal constructor(
         resourcePager.setBackgroundColor(viewModel.settings.value.effectiveBackgroundColor)
         // Let the page views handle the keyboard events.
         resourcePager.isFocusable = false
+        resourcePager.addOnPageChangeListener(PageChangeListener())
 
         parent.addView(resourcePager)
 
         resetResourcePagerAdapter()
+    }
+
+    private inner class PageChangeListener : ViewPager.SimpleOnPageChangeListener() {
+        override fun onPageSelected(position: Int) {
+            currentReflowablePageFragment?.webView?.let { webView ->
+                if (viewModel.isScrollEnabled.value) {
+                    if (currentPagerPosition < position) {
+                        // handle swipe LEFT
+                        webView.scrollToStart()
+                    } else if (currentPagerPosition > position) {
+                        // handle swipe RIGHT
+                        webView.scrollToEnd()
+                    }
+                } else {
+                    if (currentPagerPosition < position) {
+                        // handle swipe LEFT
+                        webView.setCurrentItem(0, false)
+                    } else if (currentPagerPosition > position) {
+                        // handle swipe RIGHT
+                        webView.setCurrentItem(webView.numPages - 1, false)
+                    }
+                }
+            }
+            currentPagerPosition = position // Update current position
+
+            notifyCurrentLocation()
+        }
     }
 
     private fun resetResourcePagerAdapter() {
@@ -557,9 +551,7 @@ public class EpubNavigatorFragment internal constructor(
 
     private fun invalidateResourcePager() {
         val locator = currentLocator.value
-        val listener = pageChangeListener
         resetResourcePager()
-        listener?.let { resourcePager.addOnPageChangeListener(it) }
         go(locator)
     }
 
