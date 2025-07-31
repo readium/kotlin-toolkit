eb6995c82d62ed76a712d10ffde0501ddadf2ea3
erkas <erkasraim@gmail.com>
2025-07-31 10:41:53 +0900

    refactor: migrate to viewpager2, improve fragment and state management

--- readium/navigator/src/main/java/org/readium/r2/navigator/pager/R2EpubPageFragment.kt
+++ readium/navigator/src/main/java/org/readium/r2/navigator/pager/R2EpubPageFragment.kt
@@ -348,7 +348,13 @@ internal class R2EpubPageFragment : Fragment() {
         if (view == null) return
 
         viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            // Due to the migration from ViewPager to ViewPager2,
+            // adjacent pages now transition to the RESUMED state at onPageSelected,
+            // unlike the previous behavior.
+            // Therefore, changing the lifecycle state from RESUMED to STARTED
+            // allows padding to be pre-applied to the left and right pages,
+            // ensuring consistent UI behavior during page transitions.
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 val window = activity?.window ?: return@repeatOnLifecycle
                 var top = 0
                 var bottom = 0
@@ -509,6 +515,7 @@ internal class R2EpubPageFragment : Fragment() {
 /**
  * Same as setOnClickListener, but will also report the tap point in the view.
  */
+@SuppressLint("ClickableViewAccessibility")
 private fun View.setOnClickListenerWithPoint(action: (View, PointF) -> Unit) {
     var point = PointF()
 
