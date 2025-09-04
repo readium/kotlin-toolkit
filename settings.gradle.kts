0f04d5e07b6815bb0af258430dd20ff69ec9bab7
Gustav Aaro <gustav.aaro@gmail.com>
2025-09-04 17:48:02 +0200

    Fix lint issue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
         maven(url = "https://customers.pspdfkit.com/maven")
-        maven(url = "https://jitpack.io" )
+        maven(url = "https://jitpack.io")
     }
 }
 
