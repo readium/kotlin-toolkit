5473d55f28c79b070d5d3b513b6dffc976378a91
Artem Smirnov <artem@smirnov.page>
2026-03-01 14:32:20 +0200

    Early-return instead of unwrapping nullable value to fix crashes

--- readium/navigators/web/internals/src/main/kotlin/org/readium/navigator/web/internals/pager/RenditionScrollState.kt
+++ readium/navigators/web/internals/src/main/kotlin/org/readium/navigator/web/internals/pager/RenditionScrollState.kt
@@ -183,7 +183,7 @@ public class RenditionScrollState(
             return
         }
 
-        val scrollController = pageStates[index].scrollController.value!!
+        val scrollController = pageStates[index].scrollController.value ?: return
         val scrolled = scrollController.scrollToMax(orientation)
         if (scrolled > 0) {
             dispatchRawDelta(scrolled.toFloat())
