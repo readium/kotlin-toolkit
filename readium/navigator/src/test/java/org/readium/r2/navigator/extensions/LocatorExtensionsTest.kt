2fa40c51d7dc70787b5c25078e645b384938bf6f
Morten Sjøgren <msn@nota.dk>
2026-05-05 13:13:37 +0200

    formatting LocatorExtensionsTest.kt

--- readium/navigator/src/test/java/org/readium/r2/navigator/extensions/LocatorExtensionsTest.kt
+++ readium/navigator/src/test/java/org/readium/r2/navigator/extensions/LocatorExtensionsTest.kt
@@ -12,7 +12,8 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class LocatorExtensionsTest {
     @OptIn(InternalReadiumApi::class)
-    @Test fun `get time from fragment`() {
+    @Test
+    fun `get time from fragment`() {
         assertNull(Locator.Locations(fragments = listOf("t=asd")).time)
         assertNull(Locator.Locations(fragments = listOf("t=NaN")).time)
         assertNull(Locator.Locations(fragments = listOf("t=Infinity")).time)
@@ -29,7 +30,8 @@ class LocatorExtensionsTest {
 
     }
 
-    @Test fun `htmlId from fragment`() {
+    @Test
+    fun `htmlId from fragment`() {
         assertNull(Locator.Locations().htmlId)
         assertNull(Locator.Locations(fragments = listOf("t=0.0")).htmlId)
         assertNull(Locator.Locations(fragments = listOf("page=1")).htmlId)
@@ -39,7 +41,8 @@ class LocatorExtensionsTest {
         assertEquals("chapter1", Locator.Locations(fragments = listOf("name=chapter1")).htmlId)
     }
 
-    @Test fun `page from fragment`() {
+    @Test
+    fun `page from fragment`() {
         assertNull(Locator.Locations(fragments = listOf("t=0.0")).page)
         assertNull(Locator.Locations(fragments = listOf("chapter1")).page)
         assertNull(Locator.Locations(fragments = listOf("page=NaN")).page)
@@ -49,21 +52,57 @@ class LocatorExtensionsTest {
 
     }
 
-    @Test fun `timeWithDuration from locator`() {
+    @Test
+    fun `timeWithDuration from locator`() {
         assertNull(Locator.Locations(fragments = listOf("t=asd")).timeWithDuration(120.seconds))
         assertNull(Locator.Locations(fragments = listOf("t=NaN")).timeWithDuration(120.seconds))
-        assertNull(Locator.Locations(fragments = listOf("t=Infinity")).timeWithDuration(120.seconds))
+        assertNull(
+            Locator.Locations(fragments = listOf("t=Infinity")).timeWithDuration(120.seconds)
+        )
         assertNull(Locator.Locations(fragments = listOf("t=-1.0")).timeWithDuration(120.seconds))
 
-        assertEquals(0.0.seconds, Locator.Locations(fragments = listOf("t=0.0")).timeWithDuration(120.seconds))
-        assertEquals(0.0.seconds, Locator.Locations(fragments = listOf("t=0.0"), progression = 0.0).timeWithDuration(120.seconds))
-        assertEquals(0.0.seconds, Locator.Locations(fragments = listOf("t=0"), progression = 0.0).timeWithDuration(120.seconds))
-        assertEquals(0.seconds, Locator.Locations(fragments = listOf("t=0")).timeWithDuration(120.seconds))
-        assertEquals(1.seconds, Locator.Locations(fragments = listOf("t=1")).timeWithDuration(120.seconds))
-        assertEquals(1.seconds, Locator.Locations(fragments = listOf("t=1"), progression = 1.0/120).timeWithDuration(120.seconds))
-        assertEquals(1.seconds, Locator.Locations(fragments = listOf("t=1.0")).timeWithDuration(120.seconds))
-        assertEquals(1.seconds, Locator.Locations(progression = 1.0/120).timeWithDuration(120.seconds))
-        assertEquals(1.5.seconds, Locator.Locations(fragments = listOf("t=1.5")).timeWithDuration(120.seconds))
-        assertEquals(1.5.seconds, Locator.Locations(progression = 1.5/120).timeWithDuration(120.seconds))
+        assertEquals(
+            0.0.seconds,
+            Locator.Locations(fragments = listOf("t=0.0")).timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            0.0.seconds,
+            Locator.Locations(fragments = listOf("t=0.0"), progression = 0.0)
+                .timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            0.0.seconds,
+            Locator.Locations(fragments = listOf("t=0"), progression = 0.0)
+                .timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            0.seconds,
+            Locator.Locations(fragments = listOf("t=0")).timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            1.seconds,
+            Locator.Locations(fragments = listOf("t=1")).timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            1.seconds,
+            Locator.Locations(fragments = listOf("t=1"), progression = 1.0 / 120)
+                .timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            1.seconds,
+            Locator.Locations(fragments = listOf("t=1.0")).timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            1.seconds,
+            Locator.Locations(progression = 1.0 / 120).timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            1.5.seconds,
+            Locator.Locations(fragments = listOf("t=1.5")).timeWithDuration(120.seconds)
+        )
+        assertEquals(
+            1.5.seconds,
+            Locator.Locations(progression = 1.5 / 120).timeWithDuration(120.seconds)
+        )
     }
 }
