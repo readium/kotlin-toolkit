069d1381fb5408bdf98e8b9576aa1ddb806d2b0b
Mickaël Menu <mickael.menu@gmail.com>
2026-05-13 12:32:15 +0200

    Remove `timeWithDuration`

--- readium/navigator/src/main/java/org/readium/r2/navigator/extensions/Locator.kt
+++ readium/navigator/src/main/java/org/readium/r2/navigator/extensions/Locator.kt
@@ -65,10 +65,3 @@ internal val Locator.Locations.page: Int? get() =
 @InternalReadiumApi
 public val Locator.Locations.time: Duration? get() =
     fragmentParameters["t"]?.toDoubleOrNull()?.takeIf { it.isFinite() && it >= 0.0 }?.seconds
-
-/**
- * Computes the time position from the resource duration.
- */
-internal fun Locator.Locations.timeWithDuration(duration: Duration?): Duration? =
-    let(duration, progression) { d, p -> (p * d.inWholeSeconds).seconds }
-        ?: time
