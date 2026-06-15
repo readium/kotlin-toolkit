ada8a0c78b6095e711812bf6e15bd5ff9bf29dfa
Quentin Gliosca <quentin.gliosca@gmail.com>
2026-06-15 11:40:32 +0200

    Clean up

--- readium/navigators/common/src/main/java/org/readium/navigator/common/PreferencesController.kt
+++ readium/navigators/common/src/main/java/org/readium/navigator/common/PreferencesController.kt
@@ -9,6 +9,26 @@ package org.readium.navigator.common
 import org.readium.r2.shared.ExperimentalReadiumApi
 
 /**
+ * Marker interface for the [Preferences] properties holder.
+ */
+@ExperimentalReadiumApi
+public interface Preferences<P : Preferences<P>> {
+
+    /**
+     * Creates a new instance of [P] after merging the values of [other].
+     *
+     * In case of conflict, [other] takes precedence.
+     */
+    public operator fun plus(other: P): P
+}
+
+/**
+ * Marker interface for the [Settings] properties holder.
+ */
+@ExperimentalReadiumApi
+public interface Settings
+
+/**
  * A controller for rendition settings that can be updated using preferences.
  */
 @ExperimentalReadiumApi
--- readium/navigators/common/src/main/java/org/readium/navigator/common/PreferencesEditor.kt
+++ readium/navigators/common/src/main/java/org/readium/navigator/common/PreferencesEditor.kt
@@ -7,22 +7,7 @@
 
 package org.readium.navigator.common
 
-import org.readium.r2.navigator.preferences.Configurable
 import org.readium.r2.shared.ExperimentalReadiumApi
-
-/**
- * Marker interface for the [Preferences] properties holder.
- */
-@ExperimentalReadiumApi
-public interface Preferences<P : Preferences<P>> {
-
-    /**
-     * Creates a new instance of [P] after merging the values of [other].
-     *
-     * In case of conflict, [other] takes precedence.
-     */
-    public operator fun plus(other: P): P
-}
 
 /**
  * Interactive editor of settings.
@@ -46,42 +31,4 @@ public interface PreferencesEditor<P : Preferences<P>, S : Settings> {
      * Unset all preferences.
      */
     public fun clear()
-}
-
-/**
- * JSON serializer of [P].
- */
-@ExperimentalReadiumApi
-public interface PreferencesSerializer<P : Configurable.Preferences<P>> {
-
-    /**
-     * Serialize [P] into a JSON string.
-     */
-    public fun serialize(preferences: P): String
-
-    /**
-     * Deserialize [P] from a JSON string.
-     */
-    public fun deserialize(preferences: String): P
-}
-
-/**
- * A filter to keep only some preferences and filter out some others.
- */
-@ExperimentalReadiumApi
-public fun interface PreferencesFilter<P : Preferences<P>> {
-
-    public fun filter(preferences: P): P
-}
-
-@ExperimentalReadiumApi
-public operator fun <P : Preferences<P>> PreferencesFilter<P>.plus(other: PreferencesFilter<P>): PreferencesFilter<P> =
-    CombinedPreferencesFilter(this, other)
-
-private class CombinedPreferencesFilter<P : Preferences<P>>(
-    private val inner: PreferencesFilter<P>,
-    private val outer: PreferencesFilter<P>,
-) : PreferencesFilter<P> {
-    override fun filter(preferences: P): P =
-        outer.filter(inner.filter(preferences))
 }
